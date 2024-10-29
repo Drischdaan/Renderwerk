30635ac7a792c0a2f5d91bf4275488d91bcd00a9
Drischdaan <drischdaan@gmail.com>
2024-10-29 11:25:17 +0100

    feat(engine): adds system manager

---
+++ Engine/Source/Engine/Public/Renderwerk/Engine/System.h
@@ -0,0 +1,22 @@
+﻿#pragma once
+
+#include "Renderwerk/Core/CoreAPI.h"
+
+class RENDERWERK_API ISystem
+{
+public:
+	virtual ~ISystem() = default;
+
+	DELETE_COPY_AND_MOVE(ISystem);
+
+public:
+	virtual void Initialize()
+	{
+	}
+
+	virtual void Shutdown()
+	{
+	}
+
+private:
+};
--- Engine/Source/Engine/Public/Renderwerk/EngineMinimal.h
+++ Engine/Source/Engine/Public/Renderwerk/EngineMinimal.h
@@ -1,4 +1,5 @@
 ﻿#pragma once
 
 #include "Renderwerk/Core/CoreMinimal.h"
+#include "Renderwerk/Engine/EngineFwd.h"
 #include "Renderwerk/Platform/PlatformFwd.h"
