17e746fa859d3ec9649b7aff4c7f93e326a7d789
Drischdaan <drischdaan@gmail.com>
2024-12-04 11:21:39 +0100

    feat(engine): adds memory utilities, allocator that supports stl structures and smart pointer definitions

---
+++ Engine/Source/Engine/Private/Renderwerk/Core/Memory/Memory.cpp
@@ -0,0 +1,39 @@
+﻿#include "pch.h"
+
+#include "Renderwerk/Core/Memory/Memory.h"
+
+HANDLE FMemory::ProcessHeap = GetProcessHeap();
+size64 FMemory::Usage = 0;
+
+void* FMemory::Allocate(const size64 Size, const uint8 Alignment)
+{
+	const size64 AlignedSize = CalculateAlignedSize(Size, Alignment);
+	Usage += AlignedSize;
+	return HeapAlloc(ProcessHeap, 0, AlignedSize);
+}
+
+void* FMemory::Reallocate(void* Pointer, const size64 Size, const uint8 Alignment)
+{
+	return HeapReAlloc(ProcessHeap, 0, Pointer, CalculateAlignedSize(Size, Alignment));
+}
+
+void FMemory::Copy(void* Destination, const void* Source, const size64 Size)
+{
+	CopyMemory(Destination, Source, Size);
+}
+
+void FMemory::Free(void* Pointer)
+{
+	Usage -= GetPointerSize(Pointer);
+	HeapFree(ProcessHeap, 0, Pointer);
+}
+
+size64 FMemory::CalculateAlignedSize(const size64 Size, const uint8 Alignment)
+{
+	return (Size + Alignment - 1) & ~(Alignment - 1);
+}
+
+size64 FMemory::GetPointerSize(const void* Memory)
+{
+	return HeapSize(ProcessHeap, 0, Memory);
+}
---
+++ Engine/Source/Engine/Public/Renderwerk/Core/Memory/Allocators.h
@@ -0,0 +1,55 @@
+﻿#pragma once
+// ReSharper disable CppMemberFunctionMayBeStatic
+
+#include "Renderwerk/Core/CoreTypes.h"
+#include "Renderwerk/Core/Memory/Memory.h"
+
+#include <cstddef>
+
+template <typename T>
+class FSTLAllocator
+{
+public:
+	using value_type = T;
+	using size_type = size64;
+	using difference_type = std::ptrdiff_t;
+
+	constexpr FSTLAllocator() noexcept = default;
+	constexpr FSTLAllocator(const FSTLAllocator&) noexcept = default;
+
+	template <typename TOther>
+	constexpr FSTLAllocator(const FSTLAllocator<TOther>&) noexcept
+	{
+	}
+
+	constexpr FSTLAllocator(FSTLAllocator&&) noexcept = default;
+
+	constexpr ~FSTLAllocator() = default;
+
+	constexpr FSTLAllocator& operator=(const FSTLAllocator&) = default;
+	constexpr FSTLAllocator& operator=(FSTLAllocator&&) = default;
+
+	[[nodiscard]] constexpr value_type* allocate(const size_type Count)
+	{
+		return static_cast<T*>(FMemory::Allocate(sizeof(T) * Count));
+	}
+
+	constexpr void deallocate(value_type* Pointer, size_type Count) noexcept
+	{
+		FMemory::Free(Pointer);
+	}
+
+	template <typename TOther>
+	struct rebind
+	{
+		using other = FSTLAllocator<TOther>;
+	};
+
+private:
+};
+
+template <class T, class TOther>
+bool operator==(const FSTLAllocator<T>&, const FSTLAllocator<TOther>&) { return true; }
+
+template <class T, class TOther>
+bool operator!=(const FSTLAllocator<T>&, const FSTLAllocator<TOther>&) { return false; }
