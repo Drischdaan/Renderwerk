2ff91984f3977c3cdaed4885291019f743c74124
Drischdaan <drischdaan@gmail.com>
2024-12-08 19:03:30 +0100

    feat(engine): adds include for magic enum

--- Engine/Source/Engine/Public/Renderwerk.h
+++ Engine/Source/Engine/Public/Renderwerk.h
@@ -1,4 +1,12 @@
 ﻿#pragma once
 
+// [] - Engine
+
 #include "Renderwerk/Core/CoreMinimal.h"
 #include "Renderwerk/Launch/FlowControl.h"
+
+// [] - Dependencies
+
+#define MAGIC_ENUM_USING_ALIAS_STRING_VIEW using string_view = FStringView;
+#define MAGIC_ENUM_USING_ALIAS_STRING      using string      = FString;
+#include "magic_enum/magic_enum.hpp"
--- Engine/Source/Engine/pch.h
+++ Engine/Source/Engine/pch.h
@@ -24,3 +24,9 @@
 #include "Renderwerk/Core/CoreMinimal.h"
 #include "Renderwerk/Engine/Engine.h"
 #include "Renderwerk/Launch/FlowControl.h"
+
+// [] - Dependencies
+
+#define MAGIC_ENUM_USING_ALIAS_STRING_VIEW using string_view = FStringView;
+#define MAGIC_ENUM_USING_ALIAS_STRING      using string      = FString;
+#include "magic_enum/magic_enum.hpp"
