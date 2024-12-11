4be476a53744d19a5a2c0849bb8c34b25d4be6a9
Drischdaan <drischdaan@gmail.com>
2024-12-11 22:47:55 +0100

    feat(engine): adds small improvements to buffered container

--- Engine/Source/Engine/Public/Renderwerk/Threading/BufferedContainer.h
+++ Engine/Source/Engine/Public/Renderwerk/Threading/BufferedContainer.h
@@ -5,9 +5,17 @@
 
 enum : uint8
 {
-	DEFAULT_BUFFERED_CONTAINER_BUFFER_COUNT = 2
+	DEFAULT_BUFFERED_CONTAINER_BUFFER_COUNT = 2,
 };
 
+/**
+ * A container that can be used to store items in a thread-safe manner.
+ * The container has a fixed number of buffers that can be swapped between.
+ * @note Only containers that have an emplace_back method can be used.
+ * @tparam TContainer The container type to use.
+ * @tparam TItem The item type to store.
+ * @tparam BufferCount The number of buffers to use.
+ */
 template <typename TContainer, typename TItem, size64 BufferCount = DEFAULT_BUFFERED_CONTAINER_BUFFER_COUNT>
 class TBufferedContainer
 {
@@ -21,24 +29,31 @@ public:
 	void Emplace(const TItem& Item)
 	{
 		FScopedLock Lock(Mutex);
-		Buffers[ActiveBufferIndex].emplace(Item);
+		Buffers[ActiveBufferIndex].emplace_back(Item);
 	}
 
 	void Emplace(TItem&& Item)
 	{
 		FScopedLock Lock(Mutex);
-		Buffers[ActiveBufferIndex].emplace(std::move(Item));
+		Buffers[ActiveBufferIndex].emplace_back(std::move(Item));
 	}
 
 	void SwapContainers()
 	{
 		FScopedLock Lock(Mutex);
 		ActiveBufferIndex = (ActiveBufferIndex + 1) % BufferCount;
+		Buffers.at(ActiveBufferIndex).clear();
 	}
 
 	[[nodiscard]] TContainer& GetBackContainer()
 	{
 		return Buffers.at((ActiveBufferIndex + 1) % BufferCount);
+	}
+
+	[[nodiscard]] size64 GetActiveSize()
+	{
+		FScopedLock Lock(Mutex);
+		return Buffers.at(ActiveBufferIndex).size();
 	}
 
 private:
