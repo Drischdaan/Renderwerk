7276dd1b2d7052bbd00a3321b45d17eb49327d80
Drischdaan <drischdaan@gmail.com>
2024-12-11 20:09:49 +0100

    feat(engine): adds flag type for enums

--- Engine/Source/Engine/Public/Renderwerk/Core/CoreMinimal.h
+++ Engine/Source/Engine/Public/Renderwerk/Core/CoreMinimal.h
@@ -10,4 +10,5 @@
 #include "Renderwerk/Core/Types/Name.h"
 #include "Renderwerk/Core/Types/SourceLocation.h"
 #include "Renderwerk/Core/Types/String.h"
+#include "Renderwerk/Core/Utility/Flags.h"
 #include "Renderwerk/Core/Utility/Hasher.h"
