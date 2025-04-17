342753110ef18e53ee658d39f60fbec0e22893c7
Drischdaan <drischdaan@gmail.com>
2025-04-17 08:42:13 +0200

    feat(editor): adds winmain to editor test code

--- Source/Runtime/Editor/Private/Editor/Launch.cpp
+++ Source/Runtime/Editor/Private/Editor/Launch.cpp
@@ -2,7 +2,12 @@
 
 #include "Renderwerk/Core/Test.hpp"
 
+#ifdef RW_KIND_CONSOLE_APP
 int main()
+#else
+#include <Windows.h>
+int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, PSTR lpCmdLine, int nCmdShow)
+#endif
 {
 	Test();
 	return 0;
