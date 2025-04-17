283177ea9926f755c242498532d4f655e4e1e1c3
Drischdaan <drischdaan@gmail.com>
2025-04-17 11:07:54 +0200

    feat(engine-tests): adds entrypoint

---
+++ Source/Runtime/EngineTests/Private/Launch.cpp
@@ -0,0 +1,8 @@
+﻿#include "catch_amalgamated.hpp"
+
+int main(const int ArgumentCount, char* Arguments[])
+{
+	Catch::Session Session;
+	Session.configData().allowZeroTests = true;
+	return Session.run(ArgumentCount, Arguments);
+}
