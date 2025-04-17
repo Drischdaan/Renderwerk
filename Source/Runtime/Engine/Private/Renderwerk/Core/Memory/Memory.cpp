ee120230714f52b9ff670e83ae1d02bb11fc8b7d
Drischdaan <drischdaan@gmail.com>
2025-04-17 13:42:05 +0200

    feat(engine): adds static memory class

---
+++ Source/Runtime/Engine/Private/Renderwerk/Core/Memory/Memory.cpp
@@ -0,0 +1,30 @@
+﻿#include "pch.hpp"
+
+#include "Renderwerk/Core/Memory/Memory.hpp"
+
+#include "mimalloc.h"
+
+void* FMemory::Allocate(const size64 Size, const size64 Alignment)
+{
+	return mi_malloc_aligned(Size, Alignment);
+}
+
+void* FMemory::Reallocate(void* OriginalPointer, const size64 Size, const size64 Alignment)
+{
+	return mi_realloc_aligned(OriginalPointer, Size, Alignment);
+}
+
+void FMemory::Free(void* Pointer, const size64 Alignment)
+{
+	mi_free_aligned(Pointer, Alignment);
+}
+
+bool8 FMemory::IsValidPointer(const void* Pointer)
+{
+	return mi_check_owned(Pointer);
+}
+
+size64 FMemory::GetAllocationSize(const void* Pointer)
+{
+	return mi_usable_size(Pointer);
+}
---
+++ Source/Runtime/Engine/Public/Renderwerk/Core/Memory/Memory.hpp
@@ -0,0 +1,20 @@
+﻿#pragma once
+
+#include "Renderwerk/Core/CoreAPI.hpp"
+#include "Renderwerk/Core/PrimitiveTypes.hpp"
+
+#ifndef RW_DEFAULT_ALIGNMENT
+#	define RW_DEFAULT_ALIGNMENT 8
+#endif
+
+class ENGINE_API FMemory
+{
+public:
+	static void* Allocate(size64 Size, size64 Alignment = RW_DEFAULT_ALIGNMENT);
+	static void* Reallocate(void* OriginalPointer, size64 Size, size64 Alignment = RW_DEFAULT_ALIGNMENT);
+	static void Free(void* Pointer, size64 Alignment = RW_DEFAULT_ALIGNMENT);
+
+public:
+	[[nodiscard]] static bool8 IsValidPointer(const void* Pointer);
+	[[nodiscard]] static size64 GetAllocationSize(const void* Pointer);
+};
