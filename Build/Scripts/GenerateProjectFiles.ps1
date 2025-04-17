47012b24a298e60cccc22395c71484be481cfd54
Drischdaan <drischdaan@gmail.com>
2025-04-17 08:13:18 +0200

    feat(repository): adjusts project file generation script

--- Build/Scripts/GenerateProjectFiles.ps1
+++ Build/Scripts/GenerateProjectFiles.ps1
@@ -1,3 +1,8 @@
 . .\Build\Scripts\Utility.ps1
 
+$OriginalDirectory = Get-Location
+Push-Location $PSScriptRoot\..\..
+
 ./Binaries/Tools/premake/premake5.exe --file="./Build/Config/premake5.lua" vs2022
+
+Push-Location $OriginalDirectory
