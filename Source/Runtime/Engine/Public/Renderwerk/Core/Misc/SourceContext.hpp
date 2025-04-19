28111dcf6b1e758310c3603a6e8167bede7b6648
Drischdaan <drischdaan@gmail.com>
2025-04-19 14:25:57 +0200

    feat(engine): adds source context class

---
+++ Source/Runtime/Engine/Public/Renderwerk/Core/Misc/SourceContext.hpp
@@ -0,0 +1,28 @@
+﻿#pragma once
+
+#include "Renderwerk/Core/PrimitiveTypes.hpp"
+#include "Renderwerk/Core/Containers/String.hpp"
+
+struct FSourceContext
+{
+	FStringView File;
+	uint32 Line;
+	FStringView Function;
+
+	constexpr FSourceContext(const FStringView& InFile, const uint32 InLine, const FStringView& InFunction)
+		: File(InFile), Line(InLine), Function(InFunction)
+	{
+	}
+
+	[[nodiscard]] constexpr FStringView GetFormattedFile() const
+	{
+		const size64 LastSlashIndex = File.find_last_of('\\');
+		if (LastSlashIndex != FStringView::npos)
+		{
+			return File.substr(LastSlashIndex + 1);
+		}
+		return File;
+	}
+};
+
+#define CURRENT_CONTEXT() FSourceContext(__FILEW__, __LINE__, __FUNCTIONW__)
