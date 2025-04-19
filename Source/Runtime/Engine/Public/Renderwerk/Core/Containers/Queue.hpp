76cde9f10fe7821d11c13d47439c6719df63730a
Drischdaan <drischdaan@gmail.com>
2025-04-19 15:31:54 +0200

    feat(engine): adds queue types definition

---
+++ Source/Runtime/Engine/Public/Renderwerk/Core/Containers/Queue.hpp
@@ -0,0 +1,17 @@
+﻿#pragma once
+
+#include <deque>
+#include <queue>
+
+#include "mimalloc.h"
+
+#include "Renderwerk/Core/Containers/Vector.hpp"
+
+template <typename T>
+using TDequeue = std::deque<T, mi_stl_allocator<T>>;
+
+template <typename T>
+using TQueue = std::queue<T, TDequeue<T>>;
+
+template <typename T, typename TContainer = TVector<T>, typename TComparer = std::less<typename TContainer::value_type>>
+using TPriorityQueue = std::priority_queue<T, TContainer, TComparer>;
