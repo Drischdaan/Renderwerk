60b50e784e67e6ffb28e5d041dea227066d0fce7
Drischdaan <drischdaan@gmail.com>
2025-04-19 15:32:08 +0200

    feat(engine): adds span type definition

---
+++ Source/Runtime/Engine/Public/Renderwerk/Core/Containers/Span.hpp
@@ -0,0 +1,8 @@
+﻿#pragma once
+
+#include <span>
+
+#include "Renderwerk/Core/PrimitiveTypes.hpp"
+
+template <typename T, size64 TExtent = std::dynamic_extent>
+using TSpan = std::span<T, TExtent>;
