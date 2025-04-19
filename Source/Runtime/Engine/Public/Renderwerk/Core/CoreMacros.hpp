1711d1736fdc029eb46aba6aacba12a4deab021a
Drischdaan <drischdaan@gmail.com>
2025-04-19 14:25:48 +0200

    feat(engine): adds core macro definitions

---
+++ Source/Runtime/Engine/Public/Renderwerk/Core/CoreMacros.hpp
@@ -0,0 +1,41 @@
+﻿#pragma once
+
+#define NON_COPYABLE(Class, Prefix) \
+	Prefix Class(const Class&) = delete; \
+	Prefix Class& operator=(const Class&) = delete;
+
+#define NON_MOVEABLE(Class, Prefix) \
+	Prefix Class(Class&&) = delete; \
+	Prefix Class& operator=(Class&&) = delete;
+
+#define NON_COPY_MOVEABLE(Class) \
+	NON_COPYABLE(Class, ) \
+	NON_MOVEABLE(Class, )
+
+#define NON_COPY_MOVEABLE_PREFIX(Class, Prefix) \
+	NON_COPYABLE(Class, Prefix) \
+	NON_MOVEABLE(Class, Prefix)
+
+#define DEFAULT_COPYABLE(Class, Prefix) \
+	Prefix Class(const Class&) noexcept = default; \
+	Prefix Class& operator=(const Class&) = default;
+
+#define DEFAULT_MOVEABLE(Class, Prefix) \
+	Prefix Class(Class&&) noexcept = default; \
+	Prefix Class& operator=(Class&&) = default;
+
+#define DEFAULT_COPY_MOVEABLE(Class) \
+	DEFAULT_COPYABLE(Class, ) \
+	DEFAULT_MOVEABLE(Class, )
+
+#define DEFAULT_COPY_MOVEABLE_PREFIX(Class, Prefix) \
+	DEFAULT_COPYABLE(Class, Prefix) \
+	DEFAULT_MOVEABLE(Class, Prefix)
+
+#define INNER_STRINGIFY(Value) #Value
+#define STRINGIFY(Value) INNER_STRINGIFY(Value)
+
+#define INNER_APPEND(X, Y) X##Y
+#define APPEND(X, Y) INNER_APPEND(X, Y)
+
+#define UNIQUE_VARIABLE(Prefix) APPEND(Prefix, __LINE__)
