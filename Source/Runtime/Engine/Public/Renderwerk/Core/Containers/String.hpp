ad8d6b6caadbad3d9744316c637dc41eefee5b50
Drischdaan <drischdaan@gmail.com>
2025-04-19 14:26:05 +0200

    feat(engine): adds string type definitions

---
+++ Source/Runtime/Engine/Public/Renderwerk/Core/Containers/String.hpp
@@ -0,0 +1,51 @@
+﻿#pragma once
+
+#include <format>
+#include <string>
+
+#include "mimalloc.h"
+
+#ifndef RW_USE_WIDE_STRINGS
+#	if defined(UNICODE) || defined(_UNICODE)
+#		define RW_USE_WIDE_STRINGS
+#	endif
+#endif
+
+#ifndef TEXT
+#	ifdef RW_USE_WIDE_STRINGS
+#		define TEXT_PASTE(Value) L##Value
+#	else
+#		define TEXT_PASTE(Value) ##Value
+#	endif
+#	define TEXT(Value) TEXT_PASTE(Value)
+#endif
+
+using FWideChar = wchar_t;
+using FWideString = std::basic_string<FWideChar, std::char_traits<FWideChar>, mi_stl_allocator<FWideChar>>;
+using FWideStringView = std::basic_string_view<FWideChar>;
+
+template <typename... TArguments>
+using FWideFormatString = std::wformat_string<TArguments...>;
+
+using FAnsiChar = char;
+using FAnsiString = std::basic_string<FAnsiChar, std::char_traits<FAnsiChar>, mi_stl_allocator<FAnsiChar>>;
+using FAnsiStringView = std::basic_string_view<FAnsiChar>;
+
+template <typename... TArguments>
+using FAnsiFormatString = std::format_string<TArguments...>;
+
+#ifdef RW_USE_WIDE_STRINGS
+using FChar = FWideChar;
+using FString = FWideString;
+using FStringView = FWideStringView;
+
+template <typename... TArguments>
+using FFormatString = FWideFormatString<TArguments...>;
+#else
+using FChar = FAnsiChar;
+using FString = FAnsiString;
+using FStringView = FAnsiStringView;
+
+template <typename... TArguments>
+using FFormatString = FAnsiFormatString<TArguments...>;
+#endif
