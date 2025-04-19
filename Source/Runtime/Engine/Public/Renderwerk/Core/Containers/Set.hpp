1379a363566c2a67b60c80656e8f437fad730bda
Drischdaan <drischdaan@gmail.com>
2025-04-19 15:32:01 +0200

    feat(engine): adds set types definition

---
+++ Source/Runtime/Engine/Public/Renderwerk/Core/Containers/Set.hpp
@@ -0,0 +1,12 @@
+﻿#pragma once
+
+#include <set>
+#include <unordered_set>
+
+#include "mimalloc.h"
+
+template <typename T, typename TComparer = std::less<T>>
+using TSet = std::set<T, TComparer, mi_stl_allocator<T>>;
+
+template <typename T, typename THasher = std::hash<T>, typename TComparer = std::less<T>>
+using TUnorderedSet = std::unordered_set<T, THasher, TComparer, mi_stl_allocator<T>>;
