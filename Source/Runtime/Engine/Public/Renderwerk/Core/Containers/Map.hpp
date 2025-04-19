548c07dd9297192aac04de1afd93ad6d7bf85966
Drischdaan <drischdaan@gmail.com>
2025-04-19 15:31:47 +0200

    feat(engine): adds map types definition

---
+++ Source/Runtime/Engine/Public/Renderwerk/Core/Containers/Map.hpp
@@ -0,0 +1,12 @@
+﻿#pragma once
+
+#include <map>
+#include <unordered_map>
+
+#include "mimalloc.h"
+
+template <typename TKey, typename TValue, typename TComparer = std::less<TKey>>
+using TMap = std::map<TKey, TValue, TComparer, mi_stl_allocator<std::pair<const TKey, TValue>>>;
+
+template <typename TKey, typename TValue, typename THash = std::hash<TKey>, typename TComparer = std::equal_to<TKey>>
+using TUnorderedMap = std::unordered_map<TKey, TValue, THash, TComparer, mi_stl_allocator<std::pair<const TKey, TValue>>>;
