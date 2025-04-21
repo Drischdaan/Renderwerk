be01f4c7eef1cb57648dba8bb3e29d0f295dc53d
Drischdaan <drischdaan@gmail.com>
2025-04-21 18:25:25 +0200

    feat(engine): adds condition variable  class

---
+++ Source/Runtime/Engine/Private/Renderwerk/Platform/Threading/ConditionVariable.cpp
@@ -0,0 +1,32 @@
+﻿#include "pch.hpp"
+
+#include "Renderwerk/Platform/Threading/ConditionVariable.hpp"
+#include "Renderwerk/Platform/Threading/CriticalSection.hpp"
+#include "Renderwerk/Platform/Threading/ResourceLock.hpp"
+
+FConditionVariable::FConditionVariable()
+{
+	InitializeConditionVariable(&ConditionVariable);
+}
+
+FConditionVariable::~FConditionVariable() = default;
+
+void FConditionVariable::Notify()
+{
+	WakeConditionVariable(&ConditionVariable);
+}
+
+void FConditionVariable::NotifyAll()
+{
+	WakeAllConditionVariable(&ConditionVariable);
+}
+
+bool8 FConditionVariable::Wait(FCriticalSection* Section, const uint64 WaitTime)
+{
+	return SleepConditionVariableCS(&ConditionVariable, &Section->CriticalSection, static_cast<DWORD>(WaitTime));
+}
+
+bool8 FConditionVariable::Wait(FResourceLock* ResourceLock, const uint64 WaitTime)
+{
+	return SleepConditionVariableSRW(&ConditionVariable, &ResourceLock->ResourceLock, static_cast<DWORD>(WaitTime), 0);
+}
---
+++ Source/Runtime/Engine/Public/Renderwerk/Platform/Threading/ConditionVariable.hpp
@@ -0,0 +1,41 @@
+﻿#pragma once
+
+#include "Renderwerk/Core/CoreAPI.hpp"
+#include "Renderwerk/Core/CoreMacros.hpp"
+#include "Renderwerk/Core/PrimitiveTypes.hpp"
+
+class FResourceLock;
+class FCriticalSection;
+
+class ENGINE_API FConditionVariable
+{
+public:
+	FConditionVariable();
+	~FConditionVariable();
+
+	DEFAULT_COPY_MOVEABLE(FConditionVariable)
+
+public:
+	void Notify();
+	void NotifyAll();
+
+	/**
+	 * Waits for a notification on this condition variable.
+	 *
+	 * @param Section A pointer to a critical section that MUST be locked before calling this function.
+	 *                This critical section will be automatically released while waiting and
+	 *                reacquired when the function returns.
+	 * @param WaitTime Maximum time to wait in milliseconds, or INFINITE to wait indefinitely.
+	 *                 Default is INFINITE.
+	 * @return true if the wait was satisfied by a notification, false if the wait timed out.
+	 *
+	 * @note The critical section must have been entered (locked) before calling this function.
+	 * @note This function atomically releases the critical section.
+	 */
+	[[nodiscard]] bool8 Wait(FCriticalSection* Section, uint64 WaitTime = INFINITE);
+
+	[[nodiscard]] bool8 Wait(FResourceLock* ResourceLock, uint64 WaitTime = INFINITE);
+
+private:
+	CONDITION_VARIABLE ConditionVariable;
+};
