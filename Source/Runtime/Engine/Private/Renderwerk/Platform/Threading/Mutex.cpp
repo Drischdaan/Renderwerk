6345e181cdc06663efa143e9cf302470d5d82008
Drischdaan <drischdaan@gmail.com>
2025-04-21 18:24:37 +0200

    feat(engine): adds mutex class

---
+++ Source/Runtime/Engine/Private/Renderwerk/Platform/Threading/Mutex.cpp
@@ -0,0 +1,28 @@
+﻿#include "pch.hpp"
+
+#include "Renderwerk/Platform/Threading/Mutex.hpp"
+
+FMutex::FMutex()
+{
+	MutexHandle = CreateMutex(nullptr, false, nullptr);
+}
+
+FMutex::~FMutex()
+{
+	CloseHandle(MutexHandle);
+}
+
+void FMutex::Lock()
+{
+	RW_VERIFY_MSG(WaitForSingleObject(MutexHandle, INFINITE) == WAIT_OBJECT_0, "Unable to wait for mutex handle");
+}
+
+void FMutex::Unlock()
+{
+	ReleaseMutex(MutexHandle);
+}
+
+bool8 FMutex::TryLock()
+{
+	return WaitForSingleObject(MutexHandle, 0) == WAIT_OBJECT_0;
+}
---
+++ Source/Runtime/Engine/Public/Renderwerk/Platform/Threading/Mutex.hpp
@@ -0,0 +1,24 @@
+﻿#pragma once
+
+#include <Windows.h>
+
+#include "Renderwerk/Core/CoreAPI.hpp"
+#include "Renderwerk/Platform/Threading/Lockable.hpp"
+
+class ENGINE_API FMutex : public ILockable
+{
+public:
+	FMutex();
+	~FMutex() override;
+
+	DEFAULT_COPY_MOVEABLE(FMutex)
+
+public:
+	void Lock() override;
+	void Unlock() override;
+
+	[[nodiscard]] bool8 TryLock() override;
+
+private:
+	HANDLE MutexHandle;
+};
