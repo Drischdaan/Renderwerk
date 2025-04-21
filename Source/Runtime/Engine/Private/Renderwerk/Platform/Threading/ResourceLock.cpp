5a065a0cb6692828cafa1848501882219c8272a4
Drischdaan <drischdaan@gmail.com>
2025-04-21 18:25:04 +0200

    feat(engine): adds resource lock class

---
+++ Source/Runtime/Engine/Private/Renderwerk/Platform/Threading/ResourceLock.cpp
@@ -0,0 +1,55 @@
+﻿#include "pch.hpp"
+
+#include "Renderwerk/Platform/Threading/ResourceLock.hpp"
+
+FResourceLock::FResourceLock()
+{
+	InitializeSRWLock(&ResourceLock);
+}
+
+FResourceLock::~FResourceLock() = default;
+
+void FResourceLock::Lock()
+{
+	LockWrite();
+}
+
+void FResourceLock::Unlock()
+{
+	UnlockRead();
+}
+
+bool8 FResourceLock::TryLock()
+{
+	return TryLockWrite();
+}
+
+void FResourceLock::LockRead()
+{
+	AcquireSRWLockShared(&ResourceLock);
+}
+
+void FResourceLock::UnlockRead()
+{
+	ReleaseSRWLockShared(&ResourceLock);
+}
+
+bool8 FResourceLock::TryLockRead()
+{
+	return TryAcquireSRWLockShared(&ResourceLock);
+}
+
+void FResourceLock::LockWrite()
+{
+	AcquireSRWLockExclusive(&ResourceLock);
+}
+
+void FResourceLock::UnlockWrite()
+{
+	ReleaseSRWLockExclusive(&ResourceLock);
+}
+
+bool8 FResourceLock::TryLockWrite()
+{
+	return TryAcquireSRWLockExclusive(&ResourceLock);
+}
---
+++ Source/Runtime/Engine/Public/Renderwerk/Platform/Threading/ResourceLock.hpp
@@ -0,0 +1,34 @@
+﻿#pragma once
+
+#include <Windows.h>
+
+#include "Renderwerk/Core/CoreAPI.hpp"
+#include "Renderwerk/Platform/Threading/Lockable.hpp"
+
+class ENGINE_API FResourceLock : public ILockable
+{
+public:
+	FResourceLock();
+	~FResourceLock() override;
+
+	DEFAULT_COPY_MOVEABLE(FResourceLock)
+
+public:
+	void Lock() override;
+	void Unlock() override;
+
+	[[nodiscard]] bool8 TryLock() override;
+
+	void LockRead();
+	void UnlockRead();
+	[[nodiscard]] bool8 TryLockRead();
+
+	void LockWrite();
+	void UnlockWrite();
+	[[nodiscard]] bool8 TryLockWrite();
+
+private:
+	SRWLOCK ResourceLock;
+
+	friend class ENGINE_API FConditionVariable;
+};
