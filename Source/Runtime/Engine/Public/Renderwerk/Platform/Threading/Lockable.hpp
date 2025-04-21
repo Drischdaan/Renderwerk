4dd7fbfa5f75f3faaa4ce0560deb5c65a59095fb
Drischdaan <drischdaan@gmail.com>
2025-04-21 18:24:30 +0200

    feat(engine): adds lockable interface

---
+++ Source/Runtime/Engine/Public/Renderwerk/Platform/Threading/Lockable.hpp
@@ -0,0 +1,22 @@
+﻿#pragma once
+
+#include "Renderwerk/Core/CoreAPI.hpp"
+#include "Renderwerk/Core/CoreMacros.hpp"
+#include "Renderwerk/Core/PrimitiveTypes.hpp"
+
+class ENGINE_API ILockable
+{
+public:
+	ILockable() = default;
+	virtual ~ILockable() = default;
+
+	DEFAULT_COPY_MOVEABLE(ILockable)
+
+public:
+	virtual void Lock() = 0;
+	virtual void Unlock() = 0;
+
+	[[nodiscard]] virtual bool8 TryLock() = 0;
+
+private:
+};
