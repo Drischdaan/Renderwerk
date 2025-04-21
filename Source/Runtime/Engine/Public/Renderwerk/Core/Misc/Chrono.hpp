dc201dd40a01feaec14d02a6ee83dac18508e7bd
Drischdaan <drischdaan@gmail.com>
2025-04-21 18:23:58 +0200

    feat(engine): adds chrono type definitions

---
+++ Source/Runtime/Engine/Public/Renderwerk/Core/Misc/Chrono.hpp
@@ -0,0 +1,14 @@
+﻿#pragma once
+
+#include <chrono>
+
+template <typename T, typename TPeriod= std::ratio<1>>
+using TDuration = std::chrono::duration<T, TPeriod>;
+
+using FNanosecondsDuration = std::chrono::nanoseconds;
+using FMillisecondsDuration = std::chrono::milliseconds;
+using FSecondsDuration = std::chrono::seconds;
+using FMinutesDuration = std::chrono::minutes;
+using FHoursDuration = std::chrono::hours;
+using FMonthsDuration = std::chrono::months;
+using FYearsDuration = std::chrono::years;
