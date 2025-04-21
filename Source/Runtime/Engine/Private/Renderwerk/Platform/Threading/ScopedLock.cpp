be53b58a19a4fa5c56df73ee22b2f9d744a500a2
Drischdaan <drischdaan@gmail.com>
2025-04-21 18:25:14 +0200

    feat(engine): adds scoped lock class

---
+++ Source/Runtime/Engine/Private/Renderwerk/Platform/Threading/ScopedLock.cpp
@@ -0,0 +1,31 @@
+﻿#include "pch.hpp"
+
+#include "Renderwerk/Platform/Threading/ScopedLock.hpp"
+
+#include "Renderwerk/Platform/Threading/Lockable.hpp"
+
+FScopedLock::FScopedLock(ILockable* InLockable)
+	: Lockable(InLockable)
+{
+	Lock();
+}
+
+FScopedLock::~FScopedLock()
+{
+	Unlock();
+}
+
+void FScopedLock::Lock() const
+{
+	Lockable->Lock();
+}
+
+void FScopedLock::Unlock() const
+{
+	Lockable->Unlock();
+}
+
+bool8 FScopedLock::TryLock() const
+{
+	return Lockable->TryLock();
+}
---
+++ Source/Runtime/Engine/Public/Renderwerk/Platform/Threading/ScopedLock.hpp
@@ -0,0 +1,25 @@
+﻿#pragma once
+
+#include "Renderwerk/Core/CoreAPI.hpp"
+#include "Renderwerk/Core/CoreMacros.hpp"
+#include "Renderwerk/Core/PrimitiveTypes.hpp"
+
+class ILockable;
+
+class ENGINE_API FScopedLock
+{
+public:
+	explicit FScopedLock(ILockable* InLockable);
+	~FScopedLock();
+
+	NON_COPY_MOVEABLE(FScopedLock)
+
+public:
+	void Lock() const;
+	void Unlock() const;
+
+	[[nodiscard]] bool8 TryLock() const;
+
+private:
+	ILockable* Lockable;
+};
