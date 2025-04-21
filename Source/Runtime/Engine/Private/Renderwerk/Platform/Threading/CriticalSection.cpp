f83db25b2ce7cfedfd8eea6c13505a7fc1730170
Drischdaan <drischdaan@gmail.com>
2025-04-21 18:24:52 +0200

    feat(engine): adds critical section class

---
+++ Source/Runtime/Engine/Private/Renderwerk/Platform/Threading/CriticalSection.cpp
@@ -0,0 +1,28 @@
+﻿#include "pch.hpp"
+
+#include "Renderwerk/Platform/Threading/CriticalSection.hpp"
+
+FCriticalSection::FCriticalSection()
+{
+	InitializeCriticalSectionAndSpinCount(&CriticalSection, 4000);
+}
+
+FCriticalSection::~FCriticalSection()
+{
+	DeleteCriticalSection(&CriticalSection);
+}
+
+void FCriticalSection::Lock()
+{
+	EnterCriticalSection(&CriticalSection);
+}
+
+void FCriticalSection::Unlock()
+{
+	LeaveCriticalSection(&CriticalSection);
+}
+
+bool8 FCriticalSection::TryLock()
+{
+	return TryEnterCriticalSection(&CriticalSection);
+}
---
+++ Source/Runtime/Engine/Public/Renderwerk/Platform/Threading/CriticalSection.hpp
@@ -0,0 +1,26 @@
+﻿#pragma once
+
+#include <Windows.h>
+
+#include "Renderwerk/Core/CoreAPI.hpp"
+#include "Renderwerk/Platform/Threading/Lockable.hpp"
+
+class ENGINE_API FCriticalSection : public ILockable
+{
+public:
+	FCriticalSection();
+	~FCriticalSection() override;
+
+	DEFAULT_COPY_MOVEABLE(FCriticalSection)
+
+public:
+	void Lock() override;
+	void Unlock() override;
+
+	[[nodiscard]] bool8 TryLock() override;
+
+private:
+	CRITICAL_SECTION CriticalSection;
+
+	friend class FConditionVariable;
+};
