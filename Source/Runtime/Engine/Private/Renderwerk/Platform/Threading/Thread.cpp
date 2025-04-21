8e875a392d8072b25bbfc9fcf38726c9e549c3d5
Drischdaan <drischdaan@gmail.com>
2025-04-21 18:24:16 +0200

    feat(engine): adds thread class

---
+++ Source/Runtime/Engine/Private/Renderwerk/Platform/Threading/Thread.cpp
@@ -0,0 +1,72 @@
+﻿#include "pch.hpp"
+
+#include "Renderwerk/Platform/Threading/Thread.hpp"
+
+DWORD ThreadFunc(const LPVOID ThreadParameter)
+{
+	const FThread* WindowsThread = static_cast<FThread*>(ThreadParameter);
+	if (WindowsThread->Function != nullptr)
+	{
+		return WindowsThread->Function.operator()();
+	}
+	return 0;
+}
+
+FThread::FThread(FThreadFunc&& InFunction)
+	: Function(std::move(InFunction))
+{
+}
+
+FThread::~FThread()
+{
+	if (ThreadHandle)
+	{
+		Join();
+		if (ThreadHandle)
+		{
+			Kill();
+		}
+	}
+}
+
+bool8 FThread::IsValid() const
+{
+	return ThreadHandle != nullptr;
+}
+
+void FThread::Start()
+{
+	if (State == EThreadState::Running)
+		return;
+	ThreadHandle = CreateThread(nullptr, 0, ThreadFunc, this, 0, reinterpret_cast<LPDWORD>(&Id));
+	State = EThreadState::Running;
+}
+
+void FThread::Join(const uint64 WaitTime)
+{
+	if (State != EThreadState::Running)
+		return;
+	WaitForSingleObject(ThreadHandle, static_cast<DWORD>(WaitTime));
+	CloseHandle(ThreadHandle);
+	ThreadHandle = nullptr;
+	State = EThreadState::Stopped;
+}
+
+void FThread::Kill()
+{
+	if (State != EThreadState::Running)
+		return;
+	const DWORD Result = WaitForSingleObject(ThreadHandle, 100);
+	if (Result == WAIT_TIMEOUT)
+	{
+		TerminateThread(ThreadHandle, 0);
+	}
+	CloseHandle(ThreadHandle);
+	ThreadHandle = nullptr;
+	State = EThreadState::Stopped;
+}
+
+uint64 FThread::GetCurrentId()
+{
+	return GetCurrentThreadId();
+}
---
+++ Source/Runtime/Engine/Public/Renderwerk/Platform/Threading/Thread.hpp
@@ -0,0 +1,64 @@
+﻿#pragma once
+
+#include <atomic>
+#include <functional>
+#include <Windows.h>
+
+#include "Renderwerk/Core/CoreAPI.hpp"
+#include "Renderwerk/Core/CoreMacros.hpp"
+#include "Renderwerk/Core/PrimitiveTypes.hpp"
+
+using FThreadFunc = std::function<int32()>;
+
+enum class ENGINE_API EThreadState : uint8
+{
+	Created = 0,
+	Running,
+	Stopped,
+};
+
+class ENGINE_API FThread
+{
+public:
+	FThread() = default;
+	FThread(FThreadFunc&& InFunction);
+
+	template <typename TOwner>
+	FThread(TOwner* Owner, int32 (TOwner::*InFunction)())
+		: FThread([Owner, InFunction]() -> int32 { return (Owner->*InFunction)(); })
+	{
+	}
+
+	template <typename TOwner>
+	FThread(TOwner* Owner, int32 (TOwner::*InFunction)() const)
+		: FThread([Owner, InFunction]() -> int32 { return (Owner->*InFunction)(); })
+	{
+	}
+
+	~FThread();
+
+	DEFAULT_COPY_MOVEABLE(FThread)
+
+public:
+	[[nodiscard]] bool8 IsValid() const;
+
+	void Start();
+	void Join(uint64 WaitTime = INFINITE);
+	void Kill();
+
+public:
+	[[nodiscard]] EThreadState GetState() const { return State; }
+	[[nodiscard]] uint64 GetId() const { return Id; }
+
+public:
+	[[nodiscard]] static uint64 GetCurrentId();
+
+private:
+	FThreadFunc Function = nullptr;
+	std::atomic<EThreadState> State = EThreadState::Created;
+
+	HANDLE ThreadHandle = nullptr;
+	uint64 Id = 0;
+
+	friend DWORD ThreadFunc(LPVOID ThreadParameter);
+};
