99d07a0e90d2679bb1bcf20ae9bcc0dc387c80ec
Drischdaan <drischdaan@gmail.com>
2025-04-22 19:14:28 +0200

    feat(engine): adds window module class

---
+++ Source/Runtime/Engine/Private/Renderwerk/Platform/WindowModule.cpp
@@ -0,0 +1,103 @@
+﻿#include "pch.hpp"
+
+#include <dbt.h>
+
+#include "Renderwerk/Platform/WindowModule.hpp"
+
+#include "Renderwerk/Engine/Engine.hpp"
+#include "Renderwerk/Platform/Window.hpp"
+
+LRESULT WindowProcess(const HWND WindowHandle, const UINT Message, const WPARAM WParam, const LPARAM LParam)
+{
+	FWindow* Window;
+	if (Message == WM_NCCREATE)
+	{
+		const CREATESTRUCT* CreateStruct = reinterpret_cast<CREATESTRUCT*>(LParam);
+		Window = static_cast<FWindow*>(CreateStruct->lpCreateParams);
+		SetWindowLongPtr(WindowHandle, GWLP_USERDATA, reinterpret_cast<LONG_PTR>(Window));
+	}
+	else
+	{
+		Window = reinterpret_cast<FWindow*>(GetWindowLongPtr(WindowHandle, GWLP_USERDATA));
+	}
+
+	if (Window)
+	{
+		return Window->WindowProcess(WindowHandle, Message, WParam, LParam);
+	}
+	return DefWindowProc(WindowHandle, Message, WParam, LParam);
+}
+
+FWindowModule::FWindowModule()
+	: IEngineModule(EEngineThreadAffinity::Main)
+{
+}
+
+TRef<FWindow> FWindowModule::NewWindow(const FWindowDesc& Description)
+{
+	FGuid Guid = FGuid::Generate();
+	TRef<FWindow> Window = NewRef<FWindow>(WindowClass, Guid, Description);
+	Windows.push_back(Window);
+	RW_LOG(Info, "Created window '{}' (Title: '{}')", Window->GetGuid(), Description.Title);
+	return Window;
+}
+
+void FWindowModule::DestroyWindow(const FGuid& Guid)
+{
+	const auto WindowIterator = std::ranges::find_if(Windows.begin(), Windows.end(), [Guid](const TRef<FWindow>& OtherWindow)
+	{
+		return OtherWindow->GetGuid() == Guid;
+	});
+	if (WindowIterator != Windows.end())
+	{
+		RW_LOG(Info, "Destroying window '{}' (Title: '{}')", Guid, (*WindowIterator)->GetState().Title);
+		Windows.erase(WindowIterator);
+	}
+}
+
+void FWindowModule::DestroyWindow(TRef<FWindow>&& Window)
+{
+	DestroyWindow(std::move(Window)->GetGuid());
+}
+
+TRef<FWindow> FWindowModule::GetWindow(const FGuid& Guid)
+{
+	const auto WindowIterator = std::ranges::find_if(Windows.begin(), Windows.end(), [Guid](const TRef<FWindow>& OtherWindow)
+	{
+		return OtherWindow->GetGuid() == Guid;
+	});
+	if (WindowIterator != Windows.end())
+	{
+		return *WindowIterator;
+	}
+	return nullptr;
+}
+
+void FWindowModule::Initialize()
+{
+	WindowClass = {};
+	WindowClass.cbSize = sizeof(WindowClass);
+	WindowClass.lpszClassName = TEXT("RenderwerkDefaultClass");
+	WindowClass.hInstance = GetModuleHandle(nullptr);
+	WindowClass.lpfnWndProc = WindowProcess;
+	RegisterClassEx(&WindowClass);
+
+	TickDelegateHandle = GetEngine()->GetMainThreadTickDelegate().BindRaw(this, &FWindowModule::OnTick);
+}
+
+void FWindowModule::Shutdown()
+{
+	Windows.clear();
+	GetEngine()->GetMainThreadTickDelegate().Unbind(TickDelegateHandle);
+	UnregisterClass(WindowClass.lpszClassName, WindowClass.hInstance);
+}
+
+void FWindowModule::OnTick()
+{
+	MSG Message = {};
+	while (PeekMessage(&Message, nullptr, 0, 0, PM_REMOVE))
+	{
+		TranslateMessage(&Message);
+		DispatchMessage(&Message);
+	}
+}
---
+++ Source/Runtime/Engine/Public/Renderwerk/Platform/WindowModule.hpp
@@ -0,0 +1,46 @@
+﻿#pragma once
+
+#include "Renderwerk/Core/CoreAPI.hpp"
+#include "Renderwerk/Core/Containers/Vector.hpp"
+#include "Renderwerk/Core/Delegates/MulticastDelegate.hpp"
+#include "Renderwerk/Core/Memory/SmartPointer.hpp"
+#include "Renderwerk/Core/Misc/Guid.hpp"
+#include "Renderwerk/Engine/EngineModule.hpp"
+
+struct FWindowDesc;
+class FWindow;
+class FMonitor;
+
+class ENGINE_API FWindowModule : public IEngineModule
+{
+public:
+	FWindowModule();
+	~FWindowModule() override = default;
+
+	DEFAULT_COPY_MOVEABLE(FWindowModule)
+
+public:
+	[[nodiscard]] TRef<FWindow> NewWindow(const FWindowDesc& Description);
+	void DestroyWindow(const FGuid& Guid);
+	void DestroyWindow(TRef<FWindow>&& Window);
+
+	[[nodiscard]] TRef<FWindow> GetWindow(const FGuid& Guid);
+
+private:
+	void Initialize() override;
+	void Shutdown() override;
+
+	void OnTick();
+
+public:
+	DEFINE_MODULE_NAME("Window")
+
+private:
+	WNDCLASSEX WindowClass = {};
+
+	FDelegateHandle TickDelegateHandle;
+
+	TVector<TRef<FWindow>> Windows;
+
+	friend LRESULT WindowProcess(HWND WindowHandle, UINT Message, WPARAM WParam, LPARAM LParam);
+};
