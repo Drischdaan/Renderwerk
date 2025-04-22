20f07a1339e9a5d5d629afbd44f104b23b259e0c
Drischdaan <drischdaan@gmail.com>
2025-04-22 19:14:19 +0200

    feat(engine): adds window class

---
+++ Source/Runtime/Engine/Private/Renderwerk/Platform/Window.cpp
@@ -0,0 +1,258 @@
+﻿#include "pch.hpp"
+
+#include "Renderwerk/Platform/Window.hpp"
+
+FWindow::FWindow(const WNDCLASSEX& InWindowClass, const FGuid& InGuid, const FWindowDesc& InDescription)
+	: Guid(InGuid)
+{
+	State = {};
+	State.WindowWidth = InDescription.Width;
+	State.WindowHeight = InDescription.Height;
+	State.ClientWidth = InDescription.Width;
+	State.ClientHeight = InDescription.Height;
+	State.PositionX = InDescription.PositionX;
+	State.PositionY = InDescription.PositionY;
+	State.Title = InDescription.Title;
+
+	ParentWindow = InDescription.ParentWindow;
+
+	const uint32 ExStyle = GetExWindowStyleByType(InDescription.Type);
+	const uint32 Style = GetWindowStyleByType(InDescription.Type);
+
+	if (Style & WS_BORDER)
+	{
+		RECT WindowRect = {0, 0, static_cast<LONG>(State.ClientWidth), static_cast<LONG>(State.ClientHeight)};
+		AdjustWindowRectEx(&WindowRect, Style, false, ExStyle);
+		State.WindowWidth = WindowRect.right - WindowRect.left;
+		State.WindowHeight = WindowRect.bottom - WindowRect.top;
+	}
+
+	HWND ParentWindowHandle = nullptr;
+	if (ParentWindow)
+	{
+		ParentWindowHandle = ParentWindow->WindowHandle;
+	}
+	WindowHandle = CreateWindowEx(ExStyle,
+	                              InWindowClass.lpszClassName,
+	                              State.Title.c_str(),
+	                              Style,
+	                              State.PositionX, State.PositionY,
+	                              static_cast<int32>(State.WindowWidth), static_cast<int32>(State.WindowHeight),
+	                              ParentWindowHandle,
+	                              nullptr,
+	                              InWindowClass.hInstance,
+	                              this
+	);
+}
+
+FWindow::~FWindow()
+{
+	if (WindowHandle)
+	{
+		DestroyWindow(WindowHandle);
+		WindowHandle = nullptr;
+	}
+}
+
+void FWindow::Show() const
+{
+	ShowWindowAsync(WindowHandle, SW_SHOW);
+}
+
+void FWindow::Hide() const
+{
+	ShowWindowAsync(WindowHandle, SW_HIDE);
+}
+
+void FWindow::Minimize() const
+{
+	ShowWindowAsync(WindowHandle, SW_MINIMIZE);
+}
+
+void FWindow::Maximize() const
+{
+	ShowWindowAsync(WindowHandle, SW_MAXIMIZE);
+}
+
+void FWindow::Focus() const
+{
+	SetFocus(WindowHandle);
+}
+
+void FWindow::Close() const
+{
+	CloseWindow(WindowHandle);
+}
+
+void FWindow::SetPosition(const int32 NewPositionX, const int32 NewPositionY) const
+{
+	SetWindowPos(WindowHandle, nullptr, NewPositionX, NewPositionY, 0, 0, SWP_NOSIZE);
+}
+
+void FWindow::SetWindowSize(const uint32 NewWidth, const uint32 NewHeight) const
+{
+	SetWindowPos(WindowHandle, nullptr, 0, 0, static_cast<int32>(NewWidth), static_cast<int32>(NewHeight), SWP_NOMOVE);
+}
+
+void FWindow::SetClientSize(const uint32 NewWidth, const uint32 NewHeight) const
+{
+	RECT WindowRect = {0, 0, static_cast<LONG>(NewWidth), static_cast<LONG>(NewHeight)};
+	if (AdjustWindowRect(&WindowRect, GetWindowLong(WindowHandle, GWL_STYLE), false))
+	{
+		const int32 AdjustedWidth = WindowRect.right - WindowRect.left;
+		const int32 AdjustedHeight = WindowRect.bottom - WindowRect.top;
+		SetWindowPos(WindowHandle, nullptr, 0, 0, AdjustedWidth, AdjustedHeight, SWP_NOMOVE);
+	}
+}
+
+void FWindow::SetTitle(const FString& NewTitle)
+{
+	State.Title = NewTitle;
+	SetWindowText(WindowHandle, State.Title.c_str());
+}
+
+void FWindow::AppendTitle(const FString& NewTitle)
+{
+	State.Title += NewTitle;
+	SetWindowText(WindowHandle, State.Title.c_str());
+}
+
+void FWindow::SetParentWindow(const TRef<FWindow>& NewParent)
+{
+	ParentWindow = NewParent;
+	if (ParentWindow != nullptr)
+	{
+		SetParent(WindowHandle, ParentWindow->WindowHandle);
+	}
+}
+
+LRESULT FWindow::WindowProcess(const HWND ProcessWindowHandle, const UINT Message, const WPARAM WParam, const LPARAM LParam)
+{
+	switch (Message)
+	{
+	case WM_CLOSE: return OnCloseMessage(ProcessWindowHandle);
+	case WM_DESTROY: return OnDestroyMessage();
+	case WM_SIZE: return OnSizeMessage(ProcessWindowHandle, LParam);
+	case WM_MOVE: return OnMoveMessage(LParam);
+	case WM_ENTERSIZEMOVE: return OnEnterSizeMoveMessage();
+	case WM_EXITSIZEMOVE: return OnExitSizeMoveMessage(ProcessWindowHandle);
+	default:
+		break;
+	}
+	return DefWindowProc(ProcessWindowHandle, Message, WParam, LParam);
+}
+
+int64 FWindow::OnCloseMessage(const HWND ProcessWindowHandle)
+{
+	State.bIsClosed = true;
+	DestroyWindow(ProcessWindowHandle);
+	return 0;
+}
+
+int64 FWindow::OnDestroyMessage()
+{
+	State.bIsDestroyed = true;
+	WindowHandle = nullptr;
+	return 0;
+}
+
+int64 FWindow::OnSizeMessage(const HWND ProcessWindowHandle, const LPARAM LParam)
+{
+	if (bIsInModalLoop)
+	{
+		return 0;
+	}
+	State.ClientWidth = LOWORD(LParam);
+	State.ClientHeight = HIWORD(LParam);
+
+	RECT WindowRect = {};
+	GetWindowRect(ProcessWindowHandle, &WindowRect);
+	State.WindowWidth = WindowRect.right - WindowRect.left;
+	State.WindowHeight = WindowRect.bottom - WindowRect.top;
+	return 0;
+}
+
+int64 FWindow::OnMoveMessage(const LPARAM LParam)
+{
+	if (bIsInModalLoop)
+	{
+		return 0;
+	}
+	State.PositionX = LOWORD(LParam);
+	State.PositionY = HIWORD(LParam);
+	return 0;
+}
+
+int64 FWindow::OnEnterSizeMoveMessage()
+{
+	bIsInModalLoop = true;
+	ModalState = {};
+	ModalState.Width = State.WindowWidth;
+	ModalState.Height = State.WindowHeight;
+	ModalState.PositionX = State.PositionX;
+	ModalState.PositionY = State.PositionY;
+	return 0;
+}
+
+int64 FWindow::OnExitSizeMoveMessage(const HWND ProcessWindowHandle)
+{
+	bIsInModalLoop = false;
+
+	RECT WindowRect = {};
+	GetWindowRect(ProcessWindowHandle, &WindowRect);
+
+	FModalState AfterModalState;
+	AfterModalState.Width = WindowRect.right - WindowRect.left;
+	AfterModalState.Height = WindowRect.bottom - WindowRect.top;
+	AfterModalState.PositionX = WindowRect.left;
+	AfterModalState.PositionY = WindowRect.top;
+
+	if (AfterModalState.Width != ModalState.Width || AfterModalState.Height != ModalState.Height)
+	{
+		State.WindowWidth = AfterModalState.Width;
+		State.WindowHeight = AfterModalState.Height;
+
+		RECT ClientRect = {};
+		GetClientRect(ProcessWindowHandle, &ClientRect);
+		State.ClientWidth = ClientRect.right - ClientRect.left;
+		State.ClientHeight = ClientRect.bottom - ClientRect.top;
+	}
+
+	if (AfterModalState.PositionX != ModalState.PositionX || AfterModalState.PositionY != ModalState.PositionY)
+	{
+		State.PositionX = AfterModalState.PositionX;
+		State.PositionY = AfterModalState.PositionY;
+	}
+
+	return 0;
+}
+
+uint32 FWindow::GetWindowStyleByType(const EWindowType Type)
+{
+	uint32 Style = 0;
+	switch (Type)
+	{
+	case EWindowType::Default:
+		Style |= WS_OVERLAPPEDWINDOW;
+		break;
+	case EWindowType::Tool:
+		Style |= WS_POPUP;
+		break;
+	}
+	return Style;
+}
+
+uint32 FWindow::GetExWindowStyleByType(const EWindowType Type)
+{
+	uint32 ExStyle = 0;
+	switch (Type)
+	{
+	case EWindowType::Default:
+		ExStyle |= WS_EX_APPWINDOW;
+		break;
+	case EWindowType::Tool:
+		ExStyle |= WS_EX_TOOLWINDOW;
+		break;
+	}
+	return ExStyle;
+}
---
+++ Source/Runtime/Engine/Public/Renderwerk/Platform/Window.hpp
@@ -0,0 +1,105 @@
+﻿#pragma once
+
+#include "Renderwerk/Core/CoreAPI.hpp"
+#include "Renderwerk/Core/CoreMacros.hpp"
+#include "Renderwerk/Core/Memory/SmartPointer.hpp"
+#include "Renderwerk/Core/Misc/Guid.hpp"
+
+enum class ENGINE_API EWindowType : uint8
+{
+	Default = 0,
+	Tool,
+};
+
+struct ENGINE_API FWindowDesc
+{
+	uint32 Width = 1280;
+	uint32 Height = 720;
+	int32 PositionX = 250;
+	int32 PositionY = 250;
+	EWindowType Type = EWindowType::Default;
+	FString Title = TEXT("Renderwerk");
+	TRef<class FWindow> ParentWindow = nullptr;
+};
+
+struct ENGINE_API FWindowState
+{
+	uint32 WindowWidth = 0;
+	uint32 WindowHeight = 0;
+	uint32 ClientWidth = 0;
+	uint32 ClientHeight = 0;
+	int32 PositionX = 0;
+	int32 PositionY = 0;
+	FString Title;
+	bool8 bIsClosed = false;
+	bool8 bIsDestroyed = false;
+};
+
+class ENGINE_API FWindow
+{
+private:
+	struct FModalState
+	{
+		uint32 Width = 0;
+		uint32 Height = 0;
+		int32 PositionX = 0;
+		int32 PositionY = 0;
+	};
+
+public:
+	FWindow(const WNDCLASSEX& InWindowClass, const FGuid& InGuid, const FWindowDesc& InDescription);
+	~FWindow();
+
+	DEFAULT_COPY_MOVEABLE(FWindow)
+
+public:
+	void Show() const;
+	void Hide() const;
+	void Minimize() const;
+	void Maximize() const;
+	void Focus() const;
+	void Close() const;
+
+	void SetPosition(int32 NewPositionX, int32 NewPositionY) const;
+	void SetWindowSize(uint32 NewWidth, uint32 NewHeight) const;
+	void SetClientSize(uint32 NewWidth, uint32 NewHeight) const;
+
+	void SetTitle(const FString& NewTitle);
+	void AppendTitle(const FString& NewTitle);
+
+	void SetParentWindow(const TRef<FWindow>& NewParent);
+
+public:
+	[[nodiscard]] HWND GetHandle() const { return WindowHandle; }
+
+	[[nodiscard]] FGuid GetGuid() const { return Guid; }
+
+	[[nodiscard]] FWindowState GetState() const { return State; }
+
+private:
+	LRESULT WindowProcess(HWND ProcessWindowHandle, UINT Message, WPARAM WParam, LPARAM LParam);
+
+	int64 OnCloseMessage(HWND ProcessWindowHandle);
+	int64 OnDestroyMessage();
+	int64 OnSizeMessage(HWND ProcessWindowHandle, LPARAM LParam);
+	int64 OnMoveMessage(LPARAM LParam);
+	int64 OnEnterSizeMoveMessage();
+	int64 OnExitSizeMoveMessage(HWND ProcessWindowHandle);
+
+private:
+	static uint32 GetExWindowStyleByType(EWindowType Type);
+	static uint32 GetWindowStyleByType(EWindowType Type);
+
+private:
+	FGuid Guid;
+
+	HWND WindowHandle;
+
+	FWindowState State;
+	TRef<FWindow> ParentWindow;
+
+	bool8 bIsInModalLoop = false;
+	FModalState ModalState = {};
+
+	friend LRESULT WindowProcess(HWND WindowHandle, UINT Message, WPARAM WParam, LPARAM LParam);
+};
