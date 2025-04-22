a7f1c9a95b8962a0b9b2b7d35de9254540599711
Drischdaan <drischdaan@gmail.com>
2025-04-22 19:13:42 +0200

    feat(engine): adds render thread class

---
+++ Source/Runtime/Engine/Private/Renderwerk/Engine/Threads/RenderThread.cpp
@@ -0,0 +1,39 @@
+﻿#include "pch.hpp"
+
+#include "Renderwerk/Engine/Threads/RenderThread.hpp"
+
+#include "Renderwerk/Engine/Engine.hpp"
+
+FRenderThread::FRenderThread(TAtomic<bool8>* InShouldRun)
+	: IEngineThread(TEXT("Render"), InShouldRun)
+{
+}
+
+void FRenderThread::Initialize()
+{
+	GetEngine()->MainThread->WaitForState(EEngineThreadState::Initialized);
+	{
+		const TVector<TRef<IEngineModule>> Modules = GetEngine()->GetModuleListByAffinity(EEngineThreadAffinity::Render);
+		for (const TRef<IEngineModule>& EngineModule : Modules)
+		{
+			EngineModule->Initialize();
+		}
+	}
+	RW_LOG(Info, "Render thread initialized");
+}
+
+void FRenderThread::OnTick()
+{
+}
+
+void FRenderThread::Shutdown()
+{
+	{
+		const TVector<TRef<IEngineModule>> Modules = GetEngine()->GetModuleListByAffinity(EEngineThreadAffinity::Render);
+		for (const TRef<IEngineModule>& EngineModule : Modules)
+		{
+			EngineModule->Shutdown();
+		}
+	}
+	RW_LOG(Info, "Render thread shutdown");
+}
---
+++ Source/Runtime/Engine/Public/Renderwerk/Engine/Threads/RenderThread.hpp
@@ -0,0 +1,18 @@
+﻿#pragma once
+
+#include "Renderwerk/Core/CoreAPI.hpp"
+#include "Renderwerk/Engine/EngineThread.hpp"
+
+class ENGINE_API FRenderThread : public IEngineThread
+{
+public:
+	FRenderThread(TAtomic<bool8>* InShouldRun);
+	~FRenderThread() override = default;
+
+	DEFAULT_COPY_MOVEABLE(FRenderThread)
+
+private:
+	void Initialize() override;
+	void OnTick() override;
+	void Shutdown() override;
+};
