5c7f390443382bfaf7c4370a420a7fe3db247102
Drischdaan <drischdaan@gmail.com>
2025-04-22 19:13:21 +0200

    feat(engine): adds engine thread interface

---
+++ Source/Runtime/Engine/Private/Renderwerk/Engine/EngineThread.cpp
@@ -0,0 +1,56 @@
+﻿#include "pch.hpp"
+
+#include "Renderwerk/Engine/EngineThread.hpp"
+
+#include "Renderwerk/Platform/Threading/ScopedLock.hpp"
+#include "Renderwerk/Platform/Threading/Thread.hpp"
+
+IEngineThread::IEngineThread(FString InName, TAtomic<bool8>* InShouldRun, const bool8 bUseExistingThread)
+	: Name(std::move(InName)), bShouldRun(InShouldRun)
+{
+	if (!bUseExistingThread)
+	{
+		Thread = NewOwned<FThread>([&]()
+		{
+			Initialize();
+			ChangeState(EEngineThreadState::Initialized);
+			ChangeState(EEngineThreadState::Running);
+			while (bShouldRun->load())
+			{
+				OnTick();
+			}
+			Shutdown();
+			ChangeState(EEngineThreadState::Exited);
+			return 0;
+		});
+		Thread->Start();
+	}
+}
+
+IEngineThread::~IEngineThread()
+{
+	if (Thread != nullptr)
+	{
+		Thread->Join();
+	}
+}
+
+bool8 IEngineThread::WaitForStateChange()
+{
+	FScopedLock Lock(&StateChangeSection);
+	return StateChangeVariable.Wait(&StateChangeSection);
+}
+
+void IEngineThread::WaitForState(const EEngineThreadState StateToWait)
+{
+	while (static_cast<uint8>(State.load()) < static_cast<uint8>(StateToWait))
+	{
+		WaitForStateChange();
+	}
+}
+
+void IEngineThread::ChangeState(const EEngineThreadState NewState)
+{
+	State = NewState;
+	StateChangeVariable.NotifyAll();
+}
---
+++ Source/Runtime/Engine/Public/Renderwerk/Engine/EngineThread.hpp
@@ -0,0 +1,56 @@
+﻿#pragma once
+
+#include "Renderwerk/Core/CoreAPI.hpp"
+#include "Renderwerk/Core/CoreMacros.hpp"
+#include "Renderwerk/Core/PrimitiveTypes.hpp"
+#include "Renderwerk/Core/Containers/String.hpp"
+#include "Renderwerk/Core/Memory/SmartPointer.hpp"
+#include "Renderwerk/Core/Misc/Atomic.hpp"
+#include "Renderwerk/Platform/Threading/ConditionVariable.hpp"
+#include "Renderwerk/Platform/Threading/CriticalSection.hpp"
+
+class FThread;
+
+enum class ENGINE_API EEngineThreadState : uint8
+{
+	Created = 0,
+	Initialized,
+	Running,
+	Exited,
+};
+
+class ENGINE_API IEngineThread
+{
+public:
+	IEngineThread(FString InName, TAtomic<bool8>* InShouldRun, bool8 bUseExistingThread = false);
+	virtual ~IEngineThread();
+
+	DEFAULT_COPY_MOVEABLE(IEngineThread)
+
+public:
+	bool8 WaitForStateChange();
+	void WaitForState(EEngineThreadState StateToWait);
+
+public:
+	[[nodiscard]] FString GetName() const { return Name; }
+
+	[[nodiscard]] EEngineThreadState GetState() const { return State; }
+
+private:
+	virtual void Initialize() = 0;
+	virtual void OnTick() = 0;
+	virtual void Shutdown() = 0;
+
+protected:
+	void ChangeState(EEngineThreadState NewState);
+
+protected:
+	FString Name;
+	TAtomic<bool8>* bShouldRun;
+
+	TAtomic<EEngineThreadState> State = EEngineThreadState::Created;
+	TOwned<FThread> Thread;
+
+	FCriticalSection StateChangeSection;
+	FConditionVariable StateChangeVariable;
+};
