7eb837bee155a5d3fe38238ba64685dc7e6a42e4
Drischdaan <drischdaan@gmail.com>
2025-04-22 19:13:35 +0200

    feat(engine): adds main thread class

---
+++ Source/Runtime/Engine/Private/Renderwerk/Engine/Threads/MainThread.cpp
@@ -0,0 +1,66 @@
+﻿#include "pch.hpp"
+
+#include "Renderwerk/Engine/Threads/MainThread.hpp"
+
+#include "Renderwerk/Engine/Engine.hpp"
+#include "Renderwerk/Engine/EngineModule.hpp"
+#include "Renderwerk/Platform/Window.hpp"
+#include "Renderwerk/Platform/WindowModule.hpp"
+
+FMainThread::FMainThread(TAtomic<bool8>* InShouldRun)
+	: IEngineThread(TEXT("Main"), InShouldRun, true)
+{
+}
+
+void FMainThread::Initialize()
+{
+	const TRef<FEngine> Engine = GetEngine();
+	ChangeState(EEngineThreadState::Initialized);
+	{
+		const TVector<TRef<IEngineModule>> Modules = Engine->GetModuleListByAffinity(EEngineThreadAffinity::Main);
+		for (const TRef<IEngineModule>& EngineModule : Modules)
+		{
+			EngineModule->Initialize();
+		}
+
+		const TRef<FWindowModule> WindowModule = Engine->GetModule<FWindowModule>();
+
+		const FWindowDesc Description = {};
+		Window = WindowModule->NewWindow(Description);
+		Window->Show();
+	}
+	RW_LOG(Info, "Main thread initialized");
+	ChangeState(EEngineThreadState::Running);
+}
+
+void FMainThread::OnTick()
+{
+	const TRef<FEngine> Engine = GetEngine();
+	Engine->GetMainThreadTickDelegate().Broadcast();
+
+	const FWindowState WindowState = Window->GetState();
+	if (WindowState.bIsClosed || WindowState.bIsDestroyed)
+	{
+		Engine->RequestShutdown();
+	}
+}
+
+void FMainThread::Shutdown()
+{
+	const TRef<FEngine> Engine = GetEngine();
+	Engine->RenderThread->WaitForState(EEngineThreadState::Exited);
+	Engine->UpdateThread->WaitForState(EEngineThreadState::Exited);
+	{
+		{
+			const TRef<FWindowModule> WindowModule = Engine->GetModule<FWindowModule>();
+			WindowModule->DestroyWindow(std::move(Window));
+		}
+		const TVector<TRef<IEngineModule>> Modules = Engine->GetModuleListByAffinity(EEngineThreadAffinity::Main);
+		for (const TRef<IEngineModule>& EngineModule : Modules)
+		{
+			EngineModule->Shutdown();
+		}
+	}
+	RW_LOG(Info, "Main thread shutdown");
+	ChangeState(EEngineThreadState::Exited);
+}
---
+++ Source/Runtime/Engine/Public/Renderwerk/Engine/Threads/MainThread.hpp
@@ -0,0 +1,25 @@
+﻿#pragma once
+
+#include "Renderwerk/Core/CoreAPI.hpp"
+#include "Renderwerk/Engine/EngineThread.hpp"
+
+class FWindow;
+
+class ENGINE_API FMainThread : public IEngineThread
+{
+public:
+	FMainThread(TAtomic<bool8>* InShouldRun);
+	~FMainThread() override = default;
+
+	DEFAULT_COPY_MOVEABLE(FMainThread)
+
+private:
+	void Initialize() override;
+	void OnTick() override;
+	void Shutdown() override;
+
+private:
+	TRef<FWindow> Window;
+
+	friend class ENGINE_API FEngine;
+};
