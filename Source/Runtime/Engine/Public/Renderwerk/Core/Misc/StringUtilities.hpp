b16240b4b6c0189af41a5cf883f84988325bf5dd
Drischdaan <drischdaan@gmail.com>
2025-04-23 23:28:20 +0200

    feat(engine): adds format memory sizes methods to string utilities

--- Source/Runtime/Engine/Public/Renderwerk/Core/Misc/StringUtilities.hpp
+++ Source/Runtime/Engine/Public/Renderwerk/Core/Misc/StringUtilities.hpp
@@ -4,6 +4,7 @@
 #include <type_traits>
 #include <Windows.h>
 
+#include "Renderwerk/Core/PrimitiveTypes.hpp"
 #include "Renderwerk/Core/Containers/String.hpp"
 
 class FStringUtilities
@@ -58,5 +59,59 @@ public:
 			return {};
 		}
 		return WideString;
+	}
+
+public:
+	[[nodiscard]] static FWideString FormatMemorySizeWide(const size64 Bytes)
+	{
+		const FWideChar* Units[] = {L"B", L"KB", L"MB", L"GB", L"TB", L"PB"};
+		int32 UnitIndex = 0;
+		float64 Size = static_cast<float64>(Bytes);
+		while (Size >= 1024.0 && UnitIndex < 5)
+		{
+			Size /= 1024.0;
+			UnitIndex++;
+		}
+		FWideChar Buffer[64];
+		if (UnitIndex == 0 || Size == std::floor(Size))
+		{
+			(void)swprintf(Buffer, sizeof(Buffer) / sizeof(FWideChar), L"%.0f%ls", Size, Units[UnitIndex]);
+		}
+		else
+		{
+			(void)swprintf(Buffer, sizeof(Buffer) / sizeof(FWideChar), L"%.2f%ls", Size, Units[UnitIndex]);
+		}
+		return {Buffer};
+	}
+
+	[[nodiscard]] static FAnsiString FormatMemorySizeAnsi(const size64 Bytes)
+	{
+		const FAnsiChar* Units[] = {"B", "KB", "MB", "GB", "TB", "PB"};
+		int32 unitIndex = 0;
+		float64 size = static_cast<float64>(Bytes);
+		while (size >= 1024.0 && unitIndex < 5)
+		{
+			size /= 1024.0;
+			unitIndex++;
+		}
+		FAnsiChar Buffer[64];
+		if (unitIndex == 0 || size == std::floor(size))
+		{
+			(void)snprintf(Buffer, sizeof(Buffer), "%.0f%s", size, Units[unitIndex]);
+		}
+		else
+		{
+			(void)snprintf(Buffer, sizeof(Buffer), "%.2f%s", size, Units[unitIndex]);
+		}
+		return {Buffer};
+	}
+
+	[[nodiscard]] static FString FormatMemorySize(const size64 Bytes)
+	{
+#ifdef RW_USE_WIDE_STRINGS
+		return FormatMemorySizeWide(Bytes);
+#else
+			return FormatMemorySizeAnsi(Bytes);
+#endif
 	}
 };
