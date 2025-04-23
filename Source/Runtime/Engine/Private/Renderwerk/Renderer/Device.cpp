986a851206cc7c99fc0e6d15103356fd66177dcc
Drischdaan <drischdaan@gmail.com>
2025-04-23 23:30:04 +0200

    feat(engine): adds device class

---
+++ Source/Runtime/Engine/Private/Renderwerk/Renderer/Device.cpp
@@ -0,0 +1,38 @@
+﻿#include "pch.hpp"
+
+#include "Renderwerk/Renderer/Device.hpp"
+
+#include "Renderwerk/Renderer/Adapter.hpp"
+
+FDevice::FDevice(FAdapter* InAdapter, const FDeviceDesc& InDeviceDesc)
+	: DeviceDesc(InDeviceDesc), Adapter(InAdapter)
+{
+	const HRESULT Result = D3D12CreateDevice(Adapter->GetHandle().Get(), Adapter->GetFeatureLevel(), IID_PPV_ARGS(&Device));
+	RW_VERIFY_ID(Result);
+
+	GraphicsCommandQueue = CreateInternalCommandQueue(D3D12_COMMAND_LIST_TYPE_DIRECT);
+	ComputeCommandQueue = CreateInternalCommandQueue(D3D12_COMMAND_LIST_TYPE_COMPUTE);
+	CopyCommandQueue = CreateInternalCommandQueue(D3D12_COMMAND_LIST_TYPE_COPY);
+}
+
+FDevice::~FDevice()
+{
+	CopyCommandQueue.Reset();
+	ComputeCommandQueue.Reset();
+	GraphicsCommandQueue.Reset();
+	Device.Reset();
+}
+
+TObjectHandle<ID3D12CommandQueue> FDevice::CreateInternalCommandQueue(const D3D12_COMMAND_LIST_TYPE Type) const
+{
+	D3D12_COMMAND_QUEUE_DESC CommandQueueDesc;
+	CommandQueueDesc.NodeMask = 0;
+	CommandQueueDesc.Flags = D3D12_COMMAND_QUEUE_FLAG_NONE;
+	CommandQueueDesc.Priority = D3D12_COMMAND_QUEUE_PRIORITY_HIGH;
+	CommandQueueDesc.Type = Type;
+
+	TObjectHandle<ID3D12CommandQueue> CommandQueue;
+	const HRESULT Result = Device->CreateCommandQueue(&CommandQueueDesc, IID_PPV_ARGS(&CommandQueue));
+	RW_VERIFY_ID(Result);
+	return CommandQueue;
+}
---
+++ Source/Runtime/Engine/Public/Renderwerk/Renderer/Device.hpp
@@ -0,0 +1,43 @@
+﻿#pragma once
+
+#include "Renderwerk/Core/CoreAPI.hpp"
+#include "Renderwerk/Renderer/GraphicsCommon.hpp"
+
+class FAdapter;
+
+struct ENGINE_API FDeviceDesc
+{
+	uint32 MaxRenderTargets = 100;
+};
+
+class ENGINE_API FDevice : public TGraphicsObject<FDevice>
+{
+public:
+	FDevice(FAdapter* InAdapter, const FDeviceDesc& InDeviceDesc);
+	~FDevice() override;
+
+	NON_COPY_MOVEABLE(FDevice)
+
+public:
+	[[nodiscard]] TObjectHandle<ID3D12Device14> GetHandle() const { return Device; }
+
+	[[nodiscard]] FAdapter* GetAdapter() const { return Adapter; }
+
+	[[nodiscard]] TObjectHandle<ID3D12CommandQueue> GetGraphicsCommandQueue() const { return GraphicsCommandQueue; }
+	[[nodiscard]] TObjectHandle<ID3D12CommandQueue> GetComputeCommandQueue() const { return ComputeCommandQueue; }
+	[[nodiscard]] TObjectHandle<ID3D12CommandQueue> GetCopyCommandQueue() const { return CopyCommandQueue; }
+
+private:
+	TObjectHandle<ID3D12CommandQueue> CreateInternalCommandQueue(D3D12_COMMAND_LIST_TYPE Type) const;
+
+private:
+	FDeviceDesc DeviceDesc;
+
+	FAdapter* Adapter;
+
+	TObjectHandle<ID3D12Device14> Device;
+
+	TObjectHandle<ID3D12CommandQueue> GraphicsCommandQueue;
+	TObjectHandle<ID3D12CommandQueue> ComputeCommandQueue;
+	TObjectHandle<ID3D12CommandQueue> CopyCommandQueue;
+};
