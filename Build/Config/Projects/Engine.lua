6a72ea9486f5760527dae3aa6dd2150da82fb273
Drischdaan <drischdaan@gmail.com>
2025-04-23 20:08:48 +0200

    build(repository): adds linking of directx12 libraries and includes directx headers

--- Build/Config/Projects/Engine.lua
+++ Build/Config/Projects/Engine.lua
@@ -5,3 +5,13 @@ rw_module(name)
 		rw_link_module('mimalloc')
 		rw_link_module('spdlog')
 		rw_link_module('tracy')
+
+		links({
+			'd3d12.lib',
+			'dxgi.lib',
+			'dxguid.lib',
+		})
+
+		includedirs({
+			rw_thirdparty_module_location('DirectX-Headers', 'include'),
+		})
