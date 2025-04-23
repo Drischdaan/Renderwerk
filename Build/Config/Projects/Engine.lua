8112434eebd1285506170474d107d7f88363a412
Drischdaan <drischdaan@gmail.com>
2025-04-23 20:24:14 +0200

    build(repository): adds copy command to post engine build that copy agility sdk binaries

--- Build/Config/Projects/Engine.lua
+++ Build/Config/Projects/Engine.lua
@@ -1,6 +1,15 @@
 local name = 'Engine'
 
 rw_module(name)
+
+	local agility_path = path.join(binaries_path, 'ThirdParty', 'AgilitySDK', 'build', 'native', 'bin', 'x64');
+	local agility_output_path = path.join(binaries_path, 'Engine', '%{cfg.buildcfg}', 'D3D12');
+	postbuildcommands({
+		('{MKDIR} ' .. agility_output_path),
+		('{COPY} ' .. path.join(agility_path, 'D3D12Core.dll') .. ' "' .. agility_output_path .. '"'),
+		('{COPY} ' .. path.join(agility_path, 'd3d12SDKLayers.dll') .. ' "' .. agility_output_path .. '"'),
+	})
+
 	usage('PUBLIC')
 		rw_link_module('mimalloc')
 		rw_link_module('spdlog')
