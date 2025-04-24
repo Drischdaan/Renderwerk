f9b3e7b0eb98bf740bb3ca8f3eb28411a12a9475
Drischdaan <drischdaan@gmail.com>
2025-04-24 20:12:42 +0200

    feat(engine): adds test render loop

--- Source/Runtime/Engine/Private/Renderwerk/Renderer/RendererModule.cpp
+++ Source/Runtime/Engine/Private/Renderwerk/Renderer/RendererModule.cpp
@@ -2,7 +2,25 @@
 
 #include "Renderwerk/Renderer/RendererModule.hpp"
 
+#include "Renderwerk/Engine/Engine.hpp"
 #include "Renderwerk/Profiler/Profiler.hpp"
+#include "Renderwerk/Renderer/ResourceUploader.hpp"
+
+TVector<uint8_t> CreateCheckerboardImage()
+{
+	constexpr int width = 16;
+	constexpr int height = 16;
+	TVector<uint8_t> imageData(width * height);
+	for (int y = 0; y < height; y++)
+	{
+		for (int x = 0; x < width; x++)
+		{
+			const bool isDark = (x / 4 + y / 4) % 2 == 0;
+			imageData[y * width + x] = isDark ? 50 : 200;
+		}
+	}
+	return imageData;
+}
 
 FRendererModule::FRendererModule()
 	: IEngineModule(EEngineThreadAffinity::Render)
@@ -50,16 +68,94 @@ void FRendererModule::Initialize()
 		RW_LOG(Info, "\t\t- Usage: {}", FStringUtilities::FormatMemorySize(SharedStats.UsedMemory));
 	}
 
-	constexpr FDeviceDesc DeviceDesc = {};
+	constexpr FDeviceDesc DeviceDesc = {
+#ifdef RW_CONFIG_DEBUG
+		.bEnableInfoQueue = true,
+#else
+		.bEnableInfoQueue = false,
+#endif
+	};
 	Device = Adapter->CreateDevice(DeviceDesc);
+
+	FSwapchainDesc SwapchainDesc = {};
+	SwapchainDesc.Window = GetEngine()->GetWindow();
+	Swapchain = NewObjectHandle<FSwapchain>(Device.Get(), SwapchainDesc);
+
+	Frames.resize(RW_DEFAULT_FRAME_COUNT);
+	for (uint32 Index = 0; Index < RW_DEFAULT_FRAME_COUNT; ++Index)
+	{
+		Frames[Index].CommandList = NewObjectHandle<FCommandList>(Device.Get(), D3D12_COMMAND_LIST_TYPE_DIRECT);
+		Frames[Index].Fence = Device->CreateFence();
+	}
+
+	UploadCommandList = NewObjectHandle<FCommandList>(Device.Get(), D3D12_COMMAND_LIST_TYPE_DIRECT);
+
+	// Create texture and queue upload
+	FTextureDesc TextureDesc = {};
+	TextureDesc.Width = 16;
+	TextureDesc.Height = 16;
+	TextureDesc.Usage = ETextureUsage::RenderTarget;
+	TextureDesc.MipLevels = 1;
+	TextureDesc.DebugName = TEXT("TestTexture");
+	Texture = Device->CreateTexture(TextureDesc);
+	Device->UploadTexture(Texture, CreateCheckerboardImage());
+
+	// Upload queued resources
+	const TObjectHandle<FResourceUploader> ResourceUploader = Device->GetResourceUploader();
+	UploadCommandList->Open();
+	ResourceUploader->Flush(UploadCommandList);
+	UploadCommandList->Close();
+	Device->SubmitGraphicsWork(UploadCommandList);
+	Device->FlushWork();
+	ResourceUploader->ReleaseResources();
+
+	TickDelegateHandle = GetEngine()->GetRenderThreadTickDelegate().BindRaw(this, &FRendererModule::OnTick);
 
 	RW_LOG(Info, "Renderer module initialized");
 }
 
 void FRendererModule::Shutdown()
 {
+	GetEngine()->GetRenderThreadTickDelegate().Unbind(TickDelegateHandle);
+
+	for (size64 Index = 0; Index < Frames.size(); ++Index)
+	{
+		Device->FlushWork();
+	}
+
+	UploadCommandList.Reset();
+	Frames.clear();
+	Texture.Reset();
+	Swapchain.Reset();
+
 	Device.Reset();
 	Adapter.Reset();
 	Context.Reset();
 	RW_LOG(Info, "Renderer module shutdown");
+}
+
+void FRendererModule::OnTick()
+{
+	const uint32 BackBufferIndex = Swapchain->GetBackBufferIndex();
+	const FGraphicsFrame& Frame = Frames.at(FrameIndex);
+	RW_VERIFY(Frame.Fence->Wait());
+
+	const TObjectHandle<FCommandList> CommandList = Frame.CommandList;
+	{
+		PROFILE_SCOPE("CommandListRecording");
+
+		const TObjectHandle<FTexture> BackBuffer = Swapchain->GetBackBuffer(BackBufferIndex);
+
+		CommandList->Open();
+		CommandList->ResourceBarrier(BackBuffer, D3D12_RESOURCE_STATE_RENDER_TARGET);
+		constexpr float32 Colors[4] = {1.0f, 0.5f, 0.5f, 1.0f};
+		CommandList->ClearRenderTarget(BackBuffer, Colors);
+		CommandList->SetRenderTarget(BackBuffer);
+		CommandList->ResourceBarrier(BackBuffer, D3D12_RESOURCE_STATE_PRESENT);
+		CommandList->Close();
+	}
+	Device->SubmitGraphicsWork(CommandList);
+	Swapchain->Present(false);
+	Device->SignalGraphicsQueue(Frame.Fence);
+	FrameIndex = (FrameIndex + 1) % RW_DEFAULT_FRAME_COUNT;
 }
--- Source/Runtime/Engine/Public/Renderwerk/Renderer/RendererModule.hpp
+++ Source/Runtime/Engine/Public/Renderwerk/Renderer/RendererModule.hpp
@@ -1,9 +1,21 @@
 ﻿#pragma once
 
+#include "CommandList.hpp"
+#include "Swapchain.hpp"
+
 #include "Renderwerk/Core/CoreAPI.hpp"
+#include "Renderwerk/Core/Delegates/MulticastDelegate.hpp"
 #include "Renderwerk/Engine/EngineModule.hpp"
 #include "Renderwerk/Renderer/GraphicsCommon.hpp"
 #include "Renderwerk/Renderer/GraphicsContext.hpp"
+
+#define RW_DEFAULT_FRAME_COUNT FORWARD(3)
+
+struct ENGINE_API FGraphicsFrame
+{
+	TObjectHandle<FCommandList> CommandList;
+	TObjectHandle<FFence> Fence;
+};
 
 class ENGINE_API FRendererModule : public IEngineModule
 {
@@ -17,6 +29,8 @@ private:
 	void Initialize() override;
 	void Shutdown() override;
 
+	void OnTick();
+
 public:
 	DEFINE_MODULE_NAME("Renderer")
 
@@ -25,4 +39,15 @@ private:
 
 	TObjectHandle<FAdapter> Adapter;
 	TObjectHandle<FDevice> Device;
+
+	TObjectHandle<FSwapchain> Swapchain;
+
+	TObjectHandle<FTexture> Texture;
+
+	uint32 FrameIndex = 0;
+	TVector<FGraphicsFrame> Frames;
+
+	TObjectHandle<FCommandList> UploadCommandList;
+
+	FDelegateHandle TickDelegateHandle;
 };
