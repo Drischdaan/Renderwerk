37fd2f33a3be6f650d07faebf8ba2e5abb78ee6b
Drischdaan <drischdaan@gmail.com>
2025-04-24 23:21:36 +0200

    feat(engine): adds compute pipeline class

---
+++ Source/Runtime/Engine/Public/Renderwerk/Renderer/Pipeline/ComputePipeline.hpp
@@ -0,0 +1,17 @@
+﻿#pragma once
+
+#include "Renderwerk/Core/CoreAPI.hpp"
+#include "Renderwerk/Renderer/GraphicsCommon.hpp"
+#include "Renderwerk/Renderer/ShaderCompiler.hpp"
+#include "Renderwerk/Renderer/Pipeline/Pipeline.hpp"
+
+class ENGINE_API FComputePipeline : public IPipeline
+{
+public:
+	FComputePipeline(FDevice* InDevice, const FShaderModule& Shader, const TObjectHandle<FRootSignature>& InRootSignature);
+	~FComputePipeline() override;
+
+	NON_COPY_MOVEABLE(FComputePipeline)
+
+private:
+};
