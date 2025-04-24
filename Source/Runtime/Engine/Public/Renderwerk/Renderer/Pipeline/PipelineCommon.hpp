8d459b19a255737d3124c4b9d6b80b819a891a91
Drischdaan <drischdaan@gmail.com>
2025-04-24 23:21:20 +0200

    feat(engine): adds common pipeline enums

---
+++ Source/Runtime/Engine/Public/Renderwerk/Renderer/Pipeline/PipelineCommon.hpp
@@ -0,0 +1,25 @@
+﻿#pragma once
+
+#include "Renderwerk/Renderer/GraphicsCommon.hpp"
+
+enum class ENGINE_API ECullMode : uint8
+{
+	None = D3D12_CULL_MODE_NONE,
+	Back = D3D12_CULL_MODE_BACK,
+	Front = D3D12_CULL_MODE_FRONT,
+};
+
+enum class ENGINE_API EFillMode : uint8
+{
+	Solid = D3D12_FILL_MODE_SOLID,
+	Wireframe = D3D12_FILL_MODE_WIREFRAME,
+};
+
+enum class ENGINE_API EDepthOperation : uint8
+{
+	None = D3D12_COMPARISON_FUNC_NONE,
+	Greater = D3D12_COMPARISON_FUNC_GREATER,
+	Less = D3D12_COMPARISON_FUNC_LESS,
+	Equal = D3D12_COMPARISON_FUNC_EQUAL,
+	LessEqual = D3D12_COMPARISON_FUNC_LESS_EQUAL,
+};
