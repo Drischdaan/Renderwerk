77722b535a76a9f774194dd6d3d0552578aeeb8d
Drischdaan <drischdaan@gmail.com>
2025-04-24 20:09:13 +0200

    feat(engine): adds descriptor heap class

---
+++ Source/Runtime/Engine/Private/Renderwerk/Renderer/DescriptorHeap.cpp
@@ -0,0 +1,93 @@
+﻿#include "pch.hpp"
+
+#include "Renderwerk/Renderer/DescriptorHeap.hpp"
+
+#include "Renderwerk/Platform/Threading/ScopedLock.hpp"
+#include "Renderwerk/Renderer/Device.hpp"
+
+FDescriptorHandle::FDescriptorHandle(FDescriptorHeap* InOwnerHeap, const uint32 InIndex, const CD3DX12_CPU_DESCRIPTOR_HANDLE& InCPUHandle)
+	: OwnerHeap(InOwnerHeap), Index(InIndex), CPUHandle(InCPUHandle)
+{
+}
+
+FDescriptorHandle::FDescriptorHandle(FDescriptorHeap* InOwnerHeap, const uint32 InIndex, const CD3DX12_CPU_DESCRIPTOR_HANDLE& InCPUHandle,
+                                     const CD3DX12_GPU_DESCRIPTOR_HANDLE& InGPUHandle)
+	: OwnerHeap(InOwnerHeap), Index(InIndex), CPUHandle(InCPUHandle), GPUHandle(InGPUHandle)
+{
+}
+
+bool8 FDescriptorHandle::IsValid() const
+{
+	return OwnerHeap != nullptr && CPUHandle.ptr != 0;
+}
+
+bool8 FDescriptorHandle::IsGPUVisible() const
+{
+	return GPUHandle.ptr != 0;
+}
+
+FDescriptorHeap::FDescriptorHeap(FDevice* InDevice, const FDescriptorHeapDesc& InHeapDesc)
+	: Device(InDevice), HeapDesc(InHeapDesc)
+{
+	bIsGPUVisible = HeapDesc.Type == D3D12_DESCRIPTOR_HEAP_TYPE_CBV_SRV_UAV || HeapDesc.Type == D3D12_DESCRIPTOR_HEAP_TYPE_SAMPLER;
+
+	D3D12_DESCRIPTOR_HEAP_DESC DescriptorHeapDesc;
+	DescriptorHeapDesc.NodeMask = 0;
+	DescriptorHeapDesc.Flags = bIsGPUVisible ? D3D12_DESCRIPTOR_HEAP_FLAG_SHADER_VISIBLE : D3D12_DESCRIPTOR_HEAP_FLAG_NONE;
+	DescriptorHeapDesc.Type = HeapDesc.Type;
+	DescriptorHeapDesc.NumDescriptors = HeapDesc.DescriptorCount;
+
+	const HRESULT Result = Device->GetHandle()->CreateDescriptorHeap(&DescriptorHeapDesc, IID_PPV_ARGS(&DescriptorHeap));
+	RW_VERIFY_ID(Result);
+	DescriptorHeap->SetName(HeapDesc.DebugName.data());
+
+	DescriptorSize = Device->GetHandle()->GetDescriptorHandleIncrementSize(HeapDesc.Type);
+
+	CPUStartHandle = DescriptorHeap->GetCPUDescriptorHandleForHeapStart();
+	if (bIsGPUVisible)
+	{
+		GPUStartHandle = DescriptorHeap->GetGPUDescriptorHandleForHeapStart();
+	}
+
+	for (uint32 Index = 0; Index < HeapDesc.DescriptorCount; ++Index)
+	{
+		FreeIndicesQueue.push(Index);
+	}
+}
+
+FDescriptorHeap::~FDescriptorHeap()
+{
+	DescriptorHeap.Reset();
+}
+
+FDescriptorHandle FDescriptorHeap::Allocate()
+{
+	FScopedLock Lock(&IndicesSection);
+	RW_VERIFY_MSG(!FreeIndicesQueue.empty() && AllocationCount < HeapDesc.DescriptorCount, "Descriptor heap out of free descriptors");
+
+	const uint32 Index = FreeIndicesQueue.front();
+	FreeIndicesQueue.pop();
+	++AllocationCount;
+	CD3DX12_GPU_DESCRIPTOR_HANDLE GPUHandle;
+	if (bIsGPUVisible)
+	{
+		GPUHandle.InitOffsetted(GPUStartHandle, static_cast<int32>(Index), DescriptorSize);
+	}
+	return {this, Index, CD3DX12_CPU_DESCRIPTOR_HANDLE(CPUStartHandle, static_cast<int32>(Index), DescriptorSize), GPUHandle};
+}
+
+void FDescriptorHeap::Free(FDescriptorHandle& DescriptorHandle)
+{
+	FScopedLock Lock(&IndicesSection);
+	RW_VERIFY(DescriptorHandle.OwnerHeap == this);
+	RW_VERIFY_MSG(DescriptorHandle.Index < HeapDesc.DescriptorCount, "Attempting to free an out-of-range descriptor index");
+	RW_VERIFY_MSG(DescriptorHandle.CPUHandle.ptr != 0, "Attempting to free an already freed descriptor handle");
+
+	FreeIndicesQueue.push(DescriptorHandle.Index);
+	--AllocationCount;
+
+	DescriptorHandle.OwnerHeap = nullptr;
+	DescriptorHandle.Index = 0;
+	DescriptorHandle.CPUHandle.ptr = 0;
+	DescriptorHandle.GPUHandle.ptr = 0;
+}
---
+++ Source/Runtime/Engine/Public/Renderwerk/Renderer/DescriptorHeap.hpp
@@ -0,0 +1,75 @@
+﻿#pragma once
+
+#include "Renderwerk/Core/CoreAPI.hpp"
+#include "Renderwerk/Core/PrimitiveTypes.hpp"
+#include "Renderwerk/Core/Containers/Queue.hpp"
+#include "Renderwerk/Platform/Threading/CriticalSection.hpp"
+#include "Renderwerk/Renderer/GraphicsCommon.hpp"
+
+class FDescriptorHeap;
+class FDevice;
+
+struct ENGINE_API FDescriptorHandle
+{
+public:
+	FDescriptorHandle() = default;
+	FDescriptorHandle(FDescriptorHeap* InOwnerHeap, uint32 InIndex, const CD3DX12_CPU_DESCRIPTOR_HANDLE& InCPUHandle);
+	FDescriptorHandle(FDescriptorHeap* InOwnerHeap, uint32 InIndex, const CD3DX12_CPU_DESCRIPTOR_HANDLE& InCPUHandle, const CD3DX12_GPU_DESCRIPTOR_HANDLE& InGPUHandle);
+	~FDescriptorHandle() = default;
+
+	DEFAULT_COPY_MOVEABLE(FDescriptorHandle)
+
+public:
+	[[nodiscard]] bool8 IsValid() const;
+	[[nodiscard]] bool8 IsGPUVisible() const;
+
+	[[nodiscard]] CD3DX12_CPU_DESCRIPTOR_HANDLE GetCPUHandle() const { return CPUHandle; }
+	[[nodiscard]] CD3DX12_GPU_DESCRIPTOR_HANDLE GetGPUHandle() const { return GPUHandle; }
+
+private:
+	FDescriptorHeap* OwnerHeap = nullptr;
+	uint32 Index = 0;
+
+	CD3DX12_CPU_DESCRIPTOR_HANDLE CPUHandle = {};
+	CD3DX12_GPU_DESCRIPTOR_HANDLE GPUHandle = {};
+
+	friend class FDescriptorHeap;
+};
+
+struct ENGINE_API FDescriptorHeapDesc
+{
+	D3D12_DESCRIPTOR_HEAP_TYPE Type;
+	uint32 DescriptorCount;
+	FStringView DebugName = TEXT("DescriptorHeap");
+};
+
+class ENGINE_API FDescriptorHeap : public TGraphicsObject<FDescriptorHeap>
+{
+public:
+	FDescriptorHeap(FDevice* InDevice, const FDescriptorHeapDesc& InHeapDesc);
+	~FDescriptorHeap() override;
+
+	NON_COPY_MOVEABLE(FDescriptorHeap)
+
+public:
+	[[nodiscard]] FDescriptorHandle Allocate();
+	void Free(FDescriptorHandle& DescriptorHandle);
+
+public:
+	[[nodiscard]] size64 GetAllocationCount() const { return AllocationCount; }
+
+private:
+	FDevice* Device;
+	FDescriptorHeapDesc HeapDesc;
+
+	bool8 bIsGPUVisible;
+	TObjectHandle<ID3D12DescriptorHeap> DescriptorHeap;
+
+	uint32 DescriptorSize;
+	CD3DX12_CPU_DESCRIPTOR_HANDLE CPUStartHandle;
+	CD3DX12_GPU_DESCRIPTOR_HANDLE GPUStartHandle;
+
+	FCriticalSection IndicesSection;
+	uint32 AllocationCount = 0;
+	TQueue<uint32> FreeIndicesQueue;
+};
