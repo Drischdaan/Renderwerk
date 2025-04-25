d13d704f44410a21deb14c8177a5d2af8ab6972b
Drischdaan <drischdaan@gmail.com>
2025-04-25 18:25:34 +0200

    feat(engine): adds graphics texture class

---
+++ Source/Runtime/Engine/Private/Renderwerk/Graphics/GfxTexture.cpp
@@ -0,0 +1,101 @@
+﻿#include "pch.hpp"
+
+#include "Renderwerk/Graphics/GfxTexture.hpp"
+
+#include "Renderwerk/Graphics/GfxDevice.hpp"
+
+FGfxTexture::FGfxTexture(FGfxDevice* InGfxDevice, const FGfxTextureDesc& InTextureDesc, const TComPtr<ID3D12Resource2>& InResource)
+	: FGfxTexture(InGfxDevice, InTextureDesc, InResource, TEXT("UnnamedTexture"))
+{
+}
+
+FGfxTexture::FGfxTexture(FGfxDevice* InGfxDevice, const FGfxTextureDesc& InTextureDesc, const FStringView& InDebugName)
+	: FGfxTexture(InGfxDevice, InTextureDesc, nullptr, InDebugName)
+{
+}
+
+FGfxTexture::FGfxTexture(FGfxDevice* InGfxDevice, const FGfxTextureDesc& InTextureDesc, const TComPtr<ID3D12Resource2>& InResource, const FStringView& InDebugName)
+	: IGfxResource(InGfxDevice, InDebugName), TextureDesc(InTextureDesc)
+{
+	Resource = InResource;
+	ResourceState = TextureDesc.InitialState;
+
+	ResourceDesc.Width = TextureDesc.Width;
+	ResourceDesc.Height = TextureDesc.Height;
+	ResourceDesc.Format = TextureDesc.Format;
+	ResourceDesc.MipLevels = TextureDesc.MipLevels;
+	ResourceDesc.SampleDesc.Count = 1;
+	ResourceDesc.SampleDesc.Quality = 0;
+	ResourceDesc.DepthOrArraySize = 1;
+	ResourceDesc.Alignment = D3D12_DEFAULT_RESOURCE_PLACEMENT_ALIGNMENT;
+	ResourceDesc.Layout = D3D12_TEXTURE_LAYOUT_UNKNOWN;
+	ResourceDesc.Flags = D3D12_RESOURCE_FLAG_NONE;
+
+	switch (TextureDesc.Dimension)
+	{
+	case EGfxTextureDimension::Texture1D:
+		ResourceDesc.Dimension = D3D12_RESOURCE_DIMENSION_TEXTURE1D;
+		break;
+	case EGfxTextureDimension::Texture2D:
+		ResourceDesc.Dimension = D3D12_RESOURCE_DIMENSION_TEXTURE2D;
+		break;
+	case EGfxTextureDimension::Texture3D:
+		ResourceDesc.Dimension = D3D12_RESOURCE_DIMENSION_TEXTURE3D;
+		break;
+	}
+
+	if (TextureDesc.Usage & EGfxTextureUsage::RenderTarget)
+	{
+		ResourceDesc.Flags |= D3D12_RESOURCE_FLAG_ALLOW_RENDER_TARGET;
+	}
+	if (TextureDesc.Usage & EGfxTextureUsage::Storage)
+	{
+		ResourceDesc.Flags |= D3D12_RESOURCE_FLAG_ALLOW_UNORDERED_ACCESS;
+	}
+	if (TextureDesc.Usage & EGfxTextureUsage::DepthTarget)
+	{
+		ResourceDesc.Flags |= D3D12_RESOURCE_FLAG_ALLOW_DEPTH_STENCIL;
+	}
+
+	if (!Resource)
+	{
+		CD3DX12_HEAP_PROPERTIES HeapProperties = {};
+		HeapProperties.Type = D3D12_HEAP_TYPE_DEFAULT;
+		CreateResource(&HeapProperties);
+	}
+
+	if (TextureDesc.Usage & EGfxTextureUsage::RenderTarget)
+	{
+		AllocateRenderTarget();
+	}
+}
+
+FGfxTexture::~FGfxTexture() = default;
+
+void FGfxTexture::AllocateRenderTarget()
+{
+	RTVDescriptorHandle = GfxDevice->GetRTVDescriptorHeap()->Allocate();
+
+	D3D12_RENDER_TARGET_VIEW_DESC RenderTargetViewDesc = {};
+	RenderTargetViewDesc.Format = TextureDesc.Format;
+
+	switch (TextureDesc.Dimension)
+	{
+	case EGfxTextureDimension::Texture1D:
+		RenderTargetViewDesc.ViewDimension = D3D12_RTV_DIMENSION_TEXTURE1D;
+		RenderTargetViewDesc.Texture1D.MipSlice = 0;
+		break;
+	case EGfxTextureDimension::Texture2D:
+		RenderTargetViewDesc.ViewDimension = D3D12_RTV_DIMENSION_TEXTURE2D;
+		RenderTargetViewDesc.Texture2D.MipSlice = 0;
+		RenderTargetViewDesc.Texture2D.PlaneSlice = 0;
+		break;
+	case EGfxTextureDimension::Texture3D:
+		RenderTargetViewDesc.ViewDimension = D3D12_RTV_DIMENSION_TEXTURE3D;
+		RenderTargetViewDesc.Texture3D.MipSlice = 0;
+		break;
+	}
+
+	ID3D12Device14* NativeDevice = GfxDevice->GetNativeObject<ID3D12Device14>(NativeObjectIds::D3D12_Device);
+	NativeDevice->CreateRenderTargetView(Resource.Get(), &RenderTargetViewDesc, RTVDescriptorHandle.GetCPUHandle());
+}
---
+++ Source/Runtime/Engine/Public/Renderwerk/Graphics/GfxTexture.hpp
@@ -0,0 +1,57 @@
+﻿#pragma once
+
+#include "Renderwerk/Graphics/GfxResource.hpp"
+
+#include "Renderwerk/Graphics/GfxDescriptorHeap.hpp"
+
+enum class ENGINE_API EGfxTextureUsage : uint8
+{
+	RenderTarget = BIT(1),
+	DepthTarget = BIT(2),
+	Storage = BIT(3),
+	SharedResource = BIT(4),
+};
+
+DEFINE_ENUM_FLAGS(EGfxTextureUsage)
+
+enum class ENGINE_API EGfxTextureDimension : uint8
+{
+	Texture1D = 0,
+	Texture2D,
+	Texture3D,
+};
+
+
+struct ENGINE_API FGfxTextureDesc
+{
+	EGfxTextureUsage Usage;
+	uint32 Width = 0;
+	uint32 Height = 0;
+	DXGI_FORMAT Format = DXGI_FORMAT_R8G8B8A8_UNORM;
+	uint16 MipLevels = 0;
+	EGfxTextureDimension Dimension = EGfxTextureDimension::Texture2D;
+	D3D12_RESOURCE_STATES InitialState = D3D12_RESOURCE_STATE_COMMON;
+	FStringView DebugName;
+};
+
+class ENGINE_API FGfxTexture : public IGfxResource
+{
+public:
+	explicit FGfxTexture(FGfxDevice* InGfxDevice, const FGfxTextureDesc& InTextureDesc, const TComPtr<ID3D12Resource2>& InResource = nullptr);
+	FGfxTexture(FGfxDevice* InGfxDevice, const FGfxTextureDesc& InTextureDesc, const FStringView& InDebugName);
+	FGfxTexture(FGfxDevice* InGfxDevice, const FGfxTextureDesc& InTextureDesc, const TComPtr<ID3D12Resource2>& InResource, const FStringView& InDebugName);
+	~FGfxTexture() override;
+
+	NON_COPY_MOVEABLE(FGfxTexture)
+
+public:
+	[[nodiscard]] FGfxDescriptorHandle GetRTVDescriptorHandle() const { return RTVDescriptorHandle; }
+
+private:
+	void AllocateRenderTarget();
+
+private:
+	FGfxTextureDesc TextureDesc;
+
+	FGfxDescriptorHandle RTVDescriptorHandle;
+};
