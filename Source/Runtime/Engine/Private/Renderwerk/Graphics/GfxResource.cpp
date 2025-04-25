a4b3b232a73024ea1115ada5237bb1ad96d4cee6
Drischdaan <drischdaan@gmail.com>
2025-04-25 18:25:24 +0200

    feat(engine): adds graphics resource interface

---
+++ Source/Runtime/Engine/Private/Renderwerk/Graphics/GfxResource.cpp
@@ -0,0 +1,39 @@
+﻿#include "pch.hpp"
+
+#include "Renderwerk/Graphics/GfxResource.hpp"
+
+#include "Renderwerk/Graphics/GfxDevice.hpp"
+
+IGfxResource::IGfxResource(FGfxDevice* InGfxDevice)
+	: IGfxResource(InGfxDevice, TEXT("UnnamedResource"))
+{
+}
+
+IGfxResource::IGfxResource(FGfxDevice* InGfxDevice, const FStringView& InDebugName)
+	: IGfxDeviceChild(InGfxDevice, InDebugName)
+{
+}
+
+IGfxResource::~IGfxResource()
+{
+	Resource.Reset();
+}
+
+FNativeObject IGfxResource::GetRawNativeObject(const FNativeObjectId NativeObjectId)
+{
+	if (NativeObjectId == NativeObjectIds::D3D12_Resource)
+	{
+		return Resource.Get();
+	}
+	return IGfxDeviceChild::GetRawNativeObject(NativeObjectId);
+}
+
+void IGfxResource::CreateResource(const D3D12_HEAP_PROPERTIES* HeapProperties)
+{
+	ID3D12Device14* NativeDevice = GfxDevice->GetNativeObject<ID3D12Device14>(NativeObjectIds::D3D12_Device);
+	const HRESULT Result = NativeDevice->CreateCommittedResource2(HeapProperties, D3D12_HEAP_FLAG_NONE, &ResourceDesc, ResourceState, nullptr, nullptr,
+	                                                              IID_PPV_ARGS(&Resource));
+	RW_VERIFY_ID(Result);
+
+	NativeDevice->GetCopyableFootprints1(&ResourceDesc, 0, ResourceDesc.MipLevels, 0, nullptr, nullptr, nullptr, &AllocationSize);
+}
---
+++ Source/Runtime/Engine/Public/Renderwerk/Graphics/GfxResource.hpp
@@ -0,0 +1,34 @@
+﻿#pragma once
+
+#include "Renderwerk/Graphics/GfxCommon.hpp"
+
+class ENGINE_API IGfxResource : public IGfxDeviceChild
+{
+public:
+	explicit IGfxResource(FGfxDevice* InGfxDevice);
+	IGfxResource(FGfxDevice* InGfxDevice, const FStringView& InDebugName);
+	virtual ~IGfxResource() override;
+
+	NON_COPY_MOVEABLE(IGfxResource)
+
+public:
+	[[nodiscard]] FNativeObject GetRawNativeObject(FNativeObjectId NativeObjectId) override;
+
+public:
+	[[nodiscard]] D3D12_RESOURCE_STATES GetResourceState() const { return ResourceState; }
+	[[nodiscard]] uint64 GetAllocationSize() const { return AllocationSize; }
+
+	[[nodiscard]] bool8 IsDirty() const { return bIsDirty; }
+
+protected:
+	void CreateResource(const D3D12_HEAP_PROPERTIES* HeapProperties);
+
+protected:
+	CD3DX12_RESOURCE_DESC1 ResourceDesc = {};
+	TComPtr<ID3D12Resource2> Resource;
+
+	D3D12_RESOURCE_STATES ResourceState = D3D12_RESOURCE_STATE_COMMON;
+	uint64 AllocationSize = 0;
+
+	bool8 bIsDirty = false; // Indicates if the resource changed and needs uploading
+};
