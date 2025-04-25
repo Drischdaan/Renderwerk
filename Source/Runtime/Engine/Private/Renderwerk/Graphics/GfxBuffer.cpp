f6b43a75cae66d3e825f679a99d8b65f6ec7abfa
Drischdaan <drischdaan@gmail.com>
2025-04-25 18:25:48 +0200

    feat(engine): adds graphics buffer class without implementation

---
+++ Source/Runtime/Engine/Private/Renderwerk/Graphics/GfxBuffer.cpp
@@ -0,0 +1,15 @@
+﻿#include "pch.hpp"
+
+#include "Renderwerk/Graphics/GfxBuffer.hpp"
+
+FGfxBuffer::FGfxBuffer(FGfxDevice* InGfxDevice)
+	: FGfxBuffer(InGfxDevice, TEXT("UnnamedBuffer"))
+{
+}
+
+FGfxBuffer::FGfxBuffer(FGfxDevice* InGfxDevice, const FStringView& InDebugName)
+	: IGfxResource(InGfxDevice, InDebugName)
+{
+}
+
+FGfxBuffer::~FGfxBuffer() = default;
---
+++ Source/Runtime/Engine/Public/Renderwerk/Graphics/GfxBuffer.hpp
@@ -0,0 +1,17 @@
+﻿#pragma once
+
+#include "Renderwerk/Graphics/GfxResource.hpp"
+
+class ENGINE_API FGfxBuffer : public IGfxResource
+{
+public:
+	explicit FGfxBuffer(FGfxDevice* InGfxDevice);
+	FGfxBuffer(FGfxDevice* InGfxDevice, const FStringView& InDebugName);
+	~FGfxBuffer() override;
+
+	NON_COPY_MOVEABLE(FGfxBuffer)
+
+public:
+
+private:
+};
