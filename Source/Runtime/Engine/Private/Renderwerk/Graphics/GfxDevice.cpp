a264bbe2dec8c40d34008a642637ce45f2afa084
Drischdaan <drischdaan@gmail.com>
2025-04-25 18:24:57 +0200

    feat(engine): adds graphics device class

---
+++ Source/Runtime/Engine/Private/Renderwerk/Graphics/GfxDevice.cpp
@@ -0,0 +1,86 @@
+﻿#include "pch.hpp"
+
+#include "Renderwerk/Graphics/GfxDevice.hpp"
+
+#include "Renderwerk/Graphics/GfxAdapter.hpp"
+#include "Renderwerk/Graphics/GfxDescriptorHeap.hpp"
+#include "Renderwerk/Graphics/GfxResourceManager.hpp"
+#include "Renderwerk/Graphics/GfxSwapchain.hpp"
+
+FGfxDevice::FGfxDevice(FGfxAdapter* InGfxAdapter, const FGfxDeviceDesc& InDeviceDesc)
+	: FGfxDevice(InGfxAdapter, InDeviceDesc, TEXT("UnnamedDevice"))
+{
+}
+
+FGfxDevice::FGfxDevice(FGfxAdapter* InGfxAdapter, const FGfxDeviceDesc& InDeviceDesc, const FStringView& InDebugName)
+	: IGfxAdapterChild(InGfxAdapter, InDebugName), DeviceDesc(InDeviceDesc)
+{
+	IDXGIAdapter4* Adapter = GfxAdapter->GetNativeObject<IDXGIAdapter4>(NativeObjectIds::DXGI_Adapter);
+	const HRESULT Result = D3D12CreateDevice(Adapter, GfxAdapter->GetFeatureLevel(), IID_PPV_ARGS(&Device));
+	RW_VERIFY_ID(Result);
+
+	GraphicsQueue = CreateInternalCommandQueue(D3D12_COMMAND_LIST_TYPE_DIRECT);
+	ComputeQueue = CreateInternalCommandQueue(D3D12_COMMAND_LIST_TYPE_COMPUTE);
+	CopyQueue = CreateInternalCommandQueue(D3D12_COMMAND_LIST_TYPE_COPY);
+
+	FGfxDescriptorHeapDesc RTVHeapDesc;
+	RTVHeapDesc.Type = D3D12_DESCRIPTOR_HEAP_TYPE_RTV;
+	RTVHeapDesc.DescriptorCount = DeviceDesc.MaxRenderTargets;
+	RTVDescriptorHeap = CreateDescriptorHeap(RTVHeapDesc, TEXT("RTVDescriptorHeap"));
+
+	FGfxResourceManagerDesc ResourceManagerDesc = {};
+	ResourceManager = NewRef<FGfxResourceManager>(this, ResourceManagerDesc);
+
+	RW_LOG(Debug, "Created device ('{}') for '{}' adapter with {}", GetDebugName(), GfxAdapter->GetName(), D3DUtility::ToString(GfxAdapter->GetFeatureLevel()));
+}
+
+FGfxDevice::~FGfxDevice()
+{
+	ResourceManager.reset();
+
+	RTVDescriptorHeap.reset();
+
+	CopyQueue.Reset();
+	ComputeQueue.Reset();
+	GraphicsQueue.Reset();
+
+	Device.Reset();
+}
+
+TRef<FGfxDescriptorHeap> FGfxDevice::CreateDescriptorHeap(const FGfxDescriptorHeapDesc& HeapDesc, const FStringView& DebugName)
+{
+	return NewRef<FGfxDescriptorHeap>(this, HeapDesc, DebugName);
+}
+
+TRef<FGfxSwapchain> FGfxDevice::CreateSwapchain(const FGfxSwapchainDesc& SwapchainDesc, const FStringView& DebugName)
+{
+	return NewRef<FGfxSwapchain>(this, SwapchainDesc, DebugName);
+}
+
+FNativeObject FGfxDevice::GetRawNativeObject(const FNativeObjectId NativeObjectId)
+{
+	switch (NativeObjectId)
+	{
+	case NativeObjectIds::D3D12_Device: return Device.Get();
+	case NativeObjectIds::D3D12_GraphicsCommandQueue: return GraphicsQueue.Get();
+	case NativeObjectIds::D3D12_ComputeCommandQueue: return ComputeQueue.Get();
+	case NativeObjectIds::D3D12_CopyCommandQueue: return CopyQueue.Get();
+	default:
+		break;
+	}
+	return IGfxAdapterChild::GetRawNativeObject(NativeObjectId);
+}
+
+TComPtr<ID3D12CommandQueue> FGfxDevice::CreateInternalCommandQueue(const D3D12_COMMAND_LIST_TYPE Type) const
+{
+	D3D12_COMMAND_QUEUE_DESC CommandQueueDesc;
+	CommandQueueDesc.NodeMask = 0;
+	CommandQueueDesc.Flags = D3D12_COMMAND_QUEUE_FLAG_NONE;
+	CommandQueueDesc.Priority = D3D12_COMMAND_QUEUE_PRIORITY_HIGH;
+	CommandQueueDesc.Type = Type;
+
+	TComPtr<ID3D12CommandQueue> TempCommandQueue;
+	const HRESULT Result = Device->CreateCommandQueue(&CommandQueueDesc, IID_PPV_ARGS(&TempCommandQueue));
+	RW_VERIFY_ID(Result);
+	return TempCommandQueue;
+}
---
+++ Source/Runtime/Engine/Public/Renderwerk/Graphics/GfxDevice.hpp
@@ -0,0 +1,51 @@
+﻿#pragma once
+
+#include "Renderwerk/Core/CoreAPI.hpp"
+#include "Renderwerk/Core/Memory/SmartPointer.hpp"
+#include "Renderwerk/Graphics/GfxCommon.hpp"
+
+struct ENGINE_API FGfxDeviceDesc
+{
+	uint32 MaxRenderTargets = 25;
+};
+
+class ENGINE_API FGfxDevice : public IGfxAdapterChild
+{
+public:
+	explicit FGfxDevice(FGfxAdapter* InGfxAdapter, const FGfxDeviceDesc& InDeviceDesc);
+	FGfxDevice(FGfxAdapter* InGfxAdapter, const FGfxDeviceDesc& InDeviceDesc, const FStringView& InDebugName);
+	~FGfxDevice() override;
+
+	NON_COPY_MOVEABLE(FGfxDevice)
+
+public:
+	[[nodiscard]] TRef<FGfxDescriptorHeap> CreateDescriptorHeap(const FGfxDescriptorHeapDesc& HeapDesc, const FStringView& DebugName = TEXT("UnnamedDescriptorHeap"));
+	[[nodiscard]] TRef<FGfxSwapchain> CreateSwapchain(const FGfxSwapchainDesc& SwapchainDesc, const FStringView& DebugName = TEXT("UnnamedSwapchain"));
+
+public:
+	[[nodiscard]] FNativeObject GetRawNativeObject(FNativeObjectId NativeObjectId) override;
+
+public:
+	[[nodiscard]] TComPtr<ID3D12CommandQueue> GetGraphicsQueue() const { return GraphicsQueue; }
+	[[nodiscard]] TComPtr<ID3D12CommandQueue> GetComputeQueue() const { return ComputeQueue; }
+	[[nodiscard]] TComPtr<ID3D12CommandQueue> GetCopyQueue() const { return CopyQueue; }
+
+	[[nodiscard]] TRef<FGfxDescriptorHeap> GetRTVDescriptorHeap() const { return RTVDescriptorHeap; }
+	[[nodiscard]] TRef<FGfxResourceManager> GetResourceManager() const { return ResourceManager; }
+
+private:
+	TComPtr<ID3D12CommandQueue> CreateInternalCommandQueue(D3D12_COMMAND_LIST_TYPE Type) const;
+
+private:
+	FGfxDeviceDesc DeviceDesc;
+
+	TComPtr<ID3D12Device14> Device;
+
+	TComPtr<ID3D12CommandQueue> GraphicsQueue;
+	TComPtr<ID3D12CommandQueue> ComputeQueue;
+	TComPtr<ID3D12CommandQueue> CopyQueue;
+
+	TRef<FGfxDescriptorHeap> RTVDescriptorHeap;
+
+	TRef<FGfxResourceManager> ResourceManager;
+};
