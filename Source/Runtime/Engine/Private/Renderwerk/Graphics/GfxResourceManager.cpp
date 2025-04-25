190260611f0ccea6f8ba7a994256e2e6f039225f
Drischdaan <drischdaan@gmail.com>
2025-04-25 18:25:59 +0200

    feat(engine): adds graphics resource manager class

---
+++ Source/Runtime/Engine/Private/Renderwerk/Graphics/GfxResourceManager.cpp
@@ -0,0 +1,65 @@
+﻿#include "pch.hpp"
+
+#include "Renderwerk/Graphics/GfxResourceManager.hpp"
+
+#include "Renderwerk/Graphics/GfxTexture.hpp"
+
+FGfxResourceManager::FGfxResourceManager(FGfxDevice* InGfxDevice, const FGfxResourceManagerDesc& InResourceManagerDesc)
+	: FGfxResourceManager(InGfxDevice, InResourceManagerDesc, TEXT("UnnamedResourceManager"))
+{
+}
+
+FGfxResourceManager::FGfxResourceManager(FGfxDevice* InGfxDevice, const FGfxResourceManagerDesc& InResourceManagerDesc, const FStringView& InDebugName)
+	: IGfxDeviceChild(InGfxDevice, InDebugName), ResourceManagerDesc(InResourceManagerDesc), TexturePool(InResourceManagerDesc.MaxTextures),
+	  BufferPool(InResourceManagerDesc.MaxBuffers)
+{
+}
+
+FGfxResourceManager::~FGfxResourceManager()
+{
+	TexturePool.Destroy();
+}
+
+TRef<FGfxTexture> FGfxResourceManager::AllocateTexture(const FGfxTextureDesc& TextureDesc, const FStringView& DebugName)
+{
+	RW_VERIFY_MSG(TexturePool.GetUsage() + 1 <= ResourceManagerDesc.MaxTextures, "No space to allocate new texture '{}'", DebugName);
+	FGfxTexture* TexturePointer = TexturePool.Allocate();
+	TRef<FGfxTexture> Texture = NewRefWithDeleter<FGfxTexture>(TexturePointer, FGfxTextureDeleter(this), GfxDevice, TextureDesc, DebugName);
+	Textures.push_back(Texture);
+	return Texture;
+}
+
+void FGfxResourceManager::DestroyTexture(TRef<FGfxTexture>&& Texture)
+{
+	RW_VERIFY_MSG(TexturePool.Contains(Texture.get()), "Given texture ('{}') was not created through the resource manager", Texture->GetDebugName());
+	const uint64 RefCount = Texture.use_count();
+	RW_VERIFY_MSG(RefCount == 2, "Trying to delete texture ('{}') that is still referenced", Texture->GetDebugName());
+	std::move(Texture).reset();
+	const auto Iterator = std::ranges::find(Textures.begin(), Textures.end(), Texture);
+	if (Iterator != Textures.end())
+	{
+		Textures.erase(Iterator);
+	}
+}
+
+TRef<FGfxBuffer> FGfxResourceManager::AllocateBuffer(const FStringView& DebugName)
+{
+	RW_VERIFY_MSG(BufferPool.GetUsage() + 1 <= ResourceManagerDesc.MaxBuffers, "No space to allocate new texture '{}'", DebugName);
+	FGfxBuffer* BufferPointer = BufferPool.Allocate();
+	TRef<FGfxBuffer> Buffer = NewRefWithDeleter<FGfxBuffer>(BufferPointer, FGfxBufferDeleter(this), GfxDevice, DebugName);
+	Buffers.push_back(Buffer);
+	return Buffer;
+}
+
+void FGfxResourceManager::DestroyBuffer(TRef<FGfxBuffer>&& Buffer)
+{
+	RW_VERIFY_MSG(BufferPool.Contains(Buffer.get()), "Given buffer ('{}') was not created through the resource manager", Buffer->GetDebugName());
+	const uint64 RefCount = Buffer.use_count();
+	RW_VERIFY_MSG(RefCount == 2, "Trying to delete buffer ('{}') that is still referenced", Buffer->GetDebugName());
+	std::move(Buffer).reset();
+	const auto Iterator = std::ranges::find(Buffers.begin(), Buffers.end(), Buffer);
+	if (Iterator != Buffers.end())
+	{
+		Buffers.erase(Iterator);
+	}
+}
---
+++ Source/Runtime/Engine/Public/Renderwerk/Graphics/GfxResourceManager.hpp
@@ -0,0 +1,71 @@
+﻿#pragma once
+
+#include "Renderwerk/Core/Containers/Vector.hpp"
+#include "Renderwerk/Core/Memory/PoolAllocator.hpp"
+#include "Renderwerk/Core/Memory/SmartPointer.hpp"
+#include "Renderwerk/Graphics/GfxBuffer.hpp"
+#include "Renderwerk/Graphics/GfxCommon.hpp"
+#include "Renderwerk/Graphics/GfxTexture.hpp"
+
+struct ENGINE_API FGfxResourceManagerDesc
+{
+	uint64 MaxTextures = 1024;
+	uint64 MaxBuffers = 1024;
+};
+
+class ENGINE_API FGfxResourceManager : public IGfxDeviceChild
+{
+private:
+	struct FGfxTextureDeleter
+	{
+		FGfxResourceManager* ResourceManager;
+
+		explicit FGfxTextureDeleter(FGfxResourceManager* InResourceManager)
+			: ResourceManager(InResourceManager)
+		{
+		}
+
+		void operator()(FGfxTexture* Texture) const
+		{
+			ResourceManager->TexturePool.Free(Texture);
+		}
+	};
+
+	struct FGfxBufferDeleter
+	{
+		FGfxResourceManager* ResourceManager;
+
+		explicit FGfxBufferDeleter(FGfxResourceManager* InResourceManager)
+			: ResourceManager(InResourceManager)
+		{
+		}
+
+		void operator()(FGfxBuffer* Buffer) const
+		{
+			ResourceManager->BufferPool.Free(Buffer);
+		}
+	};
+
+public:
+	explicit FGfxResourceManager(FGfxDevice* InGfxDevice, const FGfxResourceManagerDesc& InResourceManagerDesc);
+	FGfxResourceManager(FGfxDevice* InGfxDevice, const FGfxResourceManagerDesc& InResourceManagerDesc, const FStringView& InDebugName);
+	~FGfxResourceManager() override;
+
+	NON_COPY_MOVEABLE(FGfxResourceManager)
+
+public:
+	[[nodiscard]] TRef<FGfxTexture> AllocateTexture(const FGfxTextureDesc& TextureDesc, const FStringView& DebugName = TEXT("UnnamedTexture"));
+	void DestroyTexture(TRef<FGfxTexture>&& Texture);
+
+	[[nodiscard]] TRef<FGfxBuffer> AllocateBuffer(const FStringView& DebugName = TEXT("UnnamedBuffer"));
+	void DestroyBuffer(TRef<FGfxBuffer>&& Buffer);
+
+private:
+	FGfxResourceManagerDesc ResourceManagerDesc;
+
+	TPoolAllocator<FGfxTexture> TexturePool;
+	TVector<TRef<FGfxTexture>> Textures;
+
+	TPoolAllocator<FGfxBuffer> BufferPool;
+	TVector<TRef<FGfxBuffer>> Buffers;
+};
