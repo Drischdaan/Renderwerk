94e55bfa20dfaaee1abdaa00fe235979a3e5ecf8
Drischdaan <drischdaan@gmail.com>
2025-04-26 16:54:19 +0200

    feat(engine): adds graphics surface class

---
+++ Source/Runtime/Engine/Private/Renderwerk/Graphics/GfxSurface.cpp
@@ -0,0 +1,130 @@
+﻿#include "pch.hpp"
+
+#include "Renderwerk/Graphics/GfxSurface.hpp"
+
+#include "Renderwerk/Graphics/GfxCommandList.hpp"
+#include "Renderwerk/Graphics/GfxDevice.hpp"
+#include "Renderwerk/Graphics/GfxFence.hpp"
+#include "Renderwerk/Graphics/GfxSwapchain.hpp"
+#include "Renderwerk/Platform/Window.hpp"
+#include "Renderwerk/Platform/Threading/ScopedLock.hpp"
+#include "Renderwerk/Profiler/Profiler.hpp"
+
+FGfxSurface::FGfxSurface(FGfxDevice* InGfxDevice, const TRef<FWindow>& InWindow)
+	: FGfxSurface(InGfxDevice, InWindow, TEXT("UnnamedSurface"))
+{
+}
+
+FGfxSurface::FGfxSurface(FGfxDevice* InGfxDevice, const TRef<FWindow>& InWindow, const FStringView& InDebugName)
+	: IGfxDeviceChild(InGfxDevice, InDebugName), Window(InWindow)
+{
+	FGfxSwapchainDesc SwapchainDesc = {};
+	SwapchainDesc.Window = Window;
+	SwapchainDesc.BufferCount = RW_DEFAULT_BUFFER_COUNT;
+	Swapchain = GfxDevice->CreateSwapchain(SwapchainDesc);
+
+	Frames.resize(RW_DEFAULT_BUFFER_COUNT);
+	for (FGfxFrame& Frame : Frames)
+	{
+		Frame.CommandList = GfxDevice->CreateCommandList(D3D12_COMMAND_LIST_TYPE_DIRECT);
+		Frame.Fence = GfxDevice->CreateFence();
+	}
+
+	ID3D12Device14* NativeDevice = GfxDevice->GetNativeObject<ID3D12Device14>(NativeObjectIds::D3D12_Device);
+	ProfilerContext = PROFILER_RENDER_CONTEXT(NativeDevice, GfxDevice->GetGraphicsQueue().Get());
+
+	ResizeDelegateHandle = Window->GetClientResizeDelegate().BindRaw(this, &FGfxSurface::OnWindowResize);
+}
+
+FGfxSurface::~FGfxSurface()
+{
+	Window->GetClientResizeDelegate().Unbind(ResizeDelegateHandle);
+	FlushWork();
+
+	PROFILER_DESTROY_RENDER_CONTEXT(ProfilerContext);
+
+	Frames.clear();
+	Swapchain.reset();
+	Window.reset();
+}
+
+void FGfxSurface::Render()
+{
+	PROFILE_FUNCTION();
+	if (Window->GetState().bIsMinimized)
+	{
+		return;
+	}
+
+	if (bIsResizedRequested)
+	{
+		ProcessResizeRequest();
+	}
+
+	PROFILE_RENDER_FRAME(ProfilerContext);
+
+	const FGfxFrame& Frame = GetCurrentFrame();
+	RW_VERIFY(Frame.Fence->Wait());
+
+	const TRef<FGfxTexture>& BackBuffer = Swapchain->GetBackBuffer(Swapchain->GetBackBufferIndex());
+
+	const TRef<FGfxCommandList> CommandList = Frame.CommandList;
+	CommandList->Open();
+	{
+		PROFILE_RENDER_SCOPE(ProfilerContext, CommandList, "Frame")
+		CommandList->ResourceBarrier(BackBuffer, D3D12_RESOURCE_STATE_RENDER_TARGET);
+		CommandList->SetRenderTarget(BackBuffer);
+
+		constexpr float32 Color[4] = {0.1f, 0.1f, 0.1f, 1.0f};
+		CommandList->ClearRenderTarget(BackBuffer, Color);
+
+		CommandList->ResourceBarrier(BackBuffer, D3D12_RESOURCE_STATE_PRESENT);
+	}
+	CommandList->Close();
+
+	GfxDevice->SubmitGraphicsWork(CommandList);
+	Swapchain->Present(false);
+	Frame.Fence->SignalCommandQueue(GfxDevice->GetGraphicsQueue());
+
+	AdvanceFrame();
+}
+
+FGfxFrame& FGfxSurface::GetCurrentFrame()
+{
+	return Frames.at(FrameIndex);
+}
+
+void FGfxSurface::AdvanceFrame()
+{
+	FrameIndex = (FrameIndex + 1) % RW_DEFAULT_BUFFER_COUNT;
+}
+
+void FGfxSurface::FlushWork() const
+{
+	PROFILE_FUNCTION();
+	for (uint32 Index = 0; Index < RW_DEFAULT_BUFFER_COUNT; ++Index)
+	{
+		GfxDevice->FlushGraphicsQueue();
+	}
+}
+
+void FGfxSurface::OnWindowResize(const uint32 Width, const uint32 Height)
+{
+	PROFILE_FUNCTION();
+	if (Width == 0 || Height == 0)
+	{
+		RW_LOG(Trace, "Surface wasn't resized. Invalid width or height");
+		return;
+	}
+	bIsResizedRequested = true;
+	RequestedResizeWidth = Width;
+	RequestedResizeHeight = Height;
+}
+
+void FGfxSurface::ProcessResizeRequest()
+{
+	FlushWork();
+	Swapchain->Resize(RequestedResizeWidth, RequestedResizeHeight);
+	RW_LOG(Trace, "Resized surface {}x{}", RequestedResizeWidth, RequestedResizeHeight);
+	bIsResizedRequested = false;
+}
---
+++ Source/Runtime/Engine/Public/Renderwerk/Graphics/GfxSurface.hpp
@@ -0,0 +1,55 @@
+﻿#pragma once
+
+#include "Renderwerk/Core/Containers/Vector.hpp"
+#include "Renderwerk/Core/Delegates/MulticastDelegate.hpp"
+#include "Renderwerk/Core/Memory/SmartPointer.hpp"
+#include "Renderwerk/Graphics/GfxCommon.hpp"
+#include "Renderwerk/Profiler/Profiler.hpp"
+
+class FWindow;
+
+#define RW_DEFAULT_BUFFER_COUNT FORWARD(3)
+
+struct ENGINE_API FGfxFrame
+{
+	TRef<FGfxCommandList> CommandList;
+	TRef<FGfxFence> Fence;
+};
+
+class ENGINE_API FGfxSurface : public IGfxDeviceChild
+{
+public:
+	explicit FGfxSurface(FGfxDevice* InGfxDevice, const TRef<FWindow>& InWindow);
+	FGfxSurface(FGfxDevice* InGfxDevice, const TRef<FWindow>& InWindow, const FStringView& InDebugName);
+	~FGfxSurface() override;
+
+	NON_COPY_MOVEABLE(FGfxSurface)
+
+public:
+	void Render();
+
+private:
+	[[nodiscard]] FGfxFrame& GetCurrentFrame();
+	void AdvanceFrame();
+
+	void FlushWork() const;
+
+	void OnWindowResize(uint32 Width, uint32 Height);
+	void ProcessResizeRequest();
+
+private:
+	TRef<FWindow> Window;
+
+	TRef<FGfxSwapchain> Swapchain;
+
+	uint8 FrameIndex = 0;
+	TVector<FGfxFrame> Frames;
+
+	bool8 bIsResizedRequested = false;
+	uint32 RequestedResizeWidth = 0;
+	uint32 RequestedResizeHeight = 0;
+
+	FProfilerRenderContext ProfilerContext = nullptr;
+
+	FDelegateHandle ResizeDelegateHandle;
+};
