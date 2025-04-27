40ba01c5d88827bb9c7376d42bc6aa1ed7d9783a
Drischdaan <drischdaan@gmail.com>
2025-04-27 17:55:19 +0200

    feat(engine): adds graphics pipeline interface

---
+++ Source/Runtime/Engine/Public/Renderwerk/Graphics/Pipeline/GfxPipeline.hpp
@@ -0,0 +1,21 @@
+﻿#pragma once
+
+#include "Renderwerk/Core/Memory/SmartPointer.hpp"
+#include "Renderwerk/Graphics/GfxCommon.hpp"
+
+class ENGINE_API IGfxPipeline : public IGfxDeviceChild
+{
+public:
+	explicit IGfxPipeline(FGfxDevice* InGfxDevice);
+	IGfxPipeline(FGfxDevice* InGfxDevice, const FStringView& InDebugName);
+	~IGfxPipeline() override;
+
+	NON_COPY_MOVEABLE(IGfxPipeline)
+
+public:
+	[[nodiscard]] FNativeObject GetRawNativeObject(FNativeObjectId NativeObjectId) override;
+
+protected:
+	TComPtr<ID3D12PipelineState> PipelineState;
+	TRef<FGfxRootSignature> RootSignature;
+};
