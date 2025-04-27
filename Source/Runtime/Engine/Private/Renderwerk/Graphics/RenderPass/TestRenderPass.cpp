01d2d7b11132f386c8c6362422a5a4455f829b12
Drischdaan <drischdaan@gmail.com>
2025-04-27 21:51:54 +0200

    feat(engine): adds vertex and index buffers to test pipeline

--- Source/Runtime/Engine/Private/Renderwerk/Graphics/RenderPass/TestRenderPass.cpp
+++ Source/Runtime/Engine/Private/Renderwerk/Graphics/RenderPass/TestRenderPass.cpp
@@ -2,16 +2,19 @@
 
 #include "Renderwerk/Graphics/RenderPass/TestRenderPass.hpp"
 
+#include "imgui.h"
+
 #include "Renderwerk/Engine/Engine.hpp"
 #include "Renderwerk/Graphics/GfxCommandList.hpp"
 #include "Renderwerk/Graphics/GfxDevice.hpp"
 #include "Renderwerk/Graphics/GfxResourceManager.hpp"
 #include "Renderwerk/Graphics/GfxTexture.hpp"
 #include "Renderwerk/Graphics/Pipeline/GfxGraphicsPipeline.hpp"
+#include "Renderwerk/Graphics/RenderPass/GfxImguiRenderPass.hpp"
 #include "Renderwerk/Platform/Window.hpp"
 
-FTestRenderPass::FTestRenderPass(FGfxDevice* InGfxDevice, const FStringView& InDebugName)
-	: IGfxRenderPass(InGfxDevice, InDebugName)
+FTestRenderPass::FTestRenderPass(FGfxDevice* InGfxDevice)
+	: IGfxRenderPass(InGfxDevice, TEXT("TestRenderPass"))
 {
 }
 
@@ -22,16 +25,10 @@ void FTestRenderPass::CreateResources(const TRef<FGfxResourceManager>& ResourceM
 	const FAnsiString ShaderSource = R"(#pragma vertex VSMain
 #pragma pixel PSMain
 
-static const float3 VERTICES[] = {
-    float3(-0.5, -0.5, 0.0),
-    float3( 0.5, -0.5, 0.0),
-    float3( 0.0,  0.5, 0.0)
-};
-
-static const float3 COLORS[] = {
-    float3(1.0, 0.0, 0.0),
-    float3(0.0, 1.0, 0.0),
-    float3(0.0, 0.0, 1.0)
+struct VertexInput
+{
+    float3 inPos : POSITION;
+    float3 inColor : COLOR;
 };
 
 struct VertexOut
@@ -40,14 +37,17 @@ struct VertexOut
     float3 vColor : COLOR;
 };
 
-VertexOut VSMain(uint nVertexID : SV_VertexID)
+VertexOut VSMain(VertexInput vertexInput)
 {
-    VertexOut Out = (VertexOut)0;
+	float3 inColor = vertexInput.inColor;
+    float3 inPos = vertexInput.inPos;
+    float3 outColor = inColor;
+    float4 position = float4(inPos, 1.0f);
 
-    Out.vPosition = float4(VERTICES[nVertexID], 1.0);
-    Out.vColor = COLORS[nVertexID];
-
-    return Out;
+    VertexOut output;
+    output.vPosition = position;
+    output.vColor = outColor;
+    return output;
 }
 
 float4 PSMain(VertexOut Out) : SV_Target
@@ -62,10 +62,30 @@ float4 PSMain(VertexOut Out) : SV_Target
 	GraphicsPipelineDesc.Shaders[EGfxShaderType::Vertex] = VertexShader;
 	GraphicsPipelineDesc.Shaders[EGfxShaderType::Pixel] = PixelShader;
 	GraphicsPipelineDesc.Formats.push_back(DXGI_FORMAT_R8G8B8A8_UNORM);
+	GraphicsPipelineDesc.CullMode = EGfxCullMode::None;
 	GraphicsPipelineDesc.RootSignature = GfxDevice->CreateRootSignature();
+	GraphicsPipelineDesc.bUseShaderReflection = true;
 	GraphicsPipeline = NewRef<FGfxGraphicsPipeline>(GfxDevice, GraphicsPipelineDesc);
 
 	CreateRenderTarget(ResourceManager, GetEngine()->GetWindow()->GetState().ClientWidth, GetEngine()->GetWindow()->GetState().ClientHeight);
+
+
+	Vertices = {
+		{.Positon = {0.5f, -0.5f, 0.0f}, .Color = {1.0f, 0.0f, 0.0f}},
+		{.Positon = {-0.5f, -0.5f, 0.0f}, .Color = {0.0f, 1.0f, 0.0f}},
+		{.Positon = {0.0f, 0.5f, 0.0f}, .Color = {0.0f, 0.0f, 1.0f}}
+	};
+
+	constexpr size64 VertexSize = sizeof(FVertex);
+	const size64 Size = VertexSize * Vertices.size();
+	VertexBuffer = ResourceManager->AllocateBuffer(EGfxBufferType::Vertex, Size, VertexSize);
+	VertexBuffer->SetData(Vertices.data(), Size);
+
+	Indices = {0, 1, 2};
+	IndexBuffer = ResourceManager->AllocateBuffer(EGfxBufferType::Index, sizeof(uint32) * Indices.size(), sizeof(uint32));
+	IndexBuffer->SetData(Indices.data(), Indices.size());
+
+	TestHandle = FGfxImguiRenderPass::GetImguiRenderDelegate().BindRaw(this, &FTestRenderPass::OnImguiRender);
 }
 
 void FTestRenderPass::ResizeResources(const TRef<FGfxResourceManager>& ResourceManager, const uint32 NewWidth, const uint32 NewHeight)
@@ -76,6 +96,10 @@ void FTestRenderPass::ResizeResources(const TRef<FGfxResourceManager>& ResourceM
 
 void FTestRenderPass::ReleaseResources(const TRef<FGfxResourceManager>& ResourceManager)
 {
+	FGfxImguiRenderPass::GetImguiRenderDelegate().Unbind(TestHandle);
+
+	ResourceManager->DestroyBuffer(std::move(IndexBuffer));
+	ResourceManager->DestroyBuffer(std::move(VertexBuffer));
 	ResourceManager->DestroyTexture(std::move(RenderTarget));
 	GraphicsPipeline.reset();
 }
@@ -94,7 +118,19 @@ void FTestRenderPass::Render(const TRef<FGfxCommandList>& CommandList, const TRe
 	CommandList->SetViewport(RenderTarget->GetWidth(), RenderTarget->GetHeight());
 	CommandList->SetScissor(RenderTarget->GetWidth(), RenderTarget->GetHeight());
 
-	CommandList->DrawInstanced(3, 1);
+	CommandList->ResourceBarrier(VertexBuffer, D3D12_RESOURCE_STATE_VERTEX_AND_CONSTANT_BUFFER);
+	CommandList->ResourceBarrier(IndexBuffer, D3D12_RESOURCE_STATE_INDEX_BUFFER);
+
+	ID3D12GraphicsCommandList10* NativeCommandList = CommandList->GetNativeObject<ID3D12GraphicsCommandList10>(NativeObjectIds::D3D12_CommandList);
+	const D3D12_VERTEX_BUFFER_VIEW VertexBufferView = VertexBuffer->GetVertexBufferView();
+	NativeCommandList->IASetVertexBuffers(0, 1, &VertexBufferView);
+
+	const D3D12_INDEX_BUFFER_VIEW IndexBufferView = IndexBuffer->GetIndexBufferView();
+	NativeCommandList->IASetIndexBuffer(&IndexBufferView);
+	NativeCommandList->DrawIndexedInstanced(3, 1, 0, 0, 0);
+
+	CommandList->ResourceBarrier(VertexBuffer, D3D12_RESOURCE_STATE_COMMON);
+	CommandList->ResourceBarrier(IndexBuffer, D3D12_RESOURCE_STATE_COMMON);
 
 	CommandList->ResourceBarrier(RenderTarget, D3D12_RESOURCE_STATE_COPY_SOURCE);
 	CommandList->ResourceBarrier(BackBuffer, D3D12_RESOURCE_STATE_COPY_DEST);
@@ -108,4 +144,74 @@ void FTestRenderPass::CreateRenderTarget(const TRef<FGfxResourceManager>& Resour
 	TextureDesc.Height = Height;
 	TextureDesc.Usage = EGfxTextureUsage::RenderTarget;
 	RenderTarget = ResourceManager->AllocateTexture(TextureDesc, TEXT("TestPassRenderTarget"));
+}
+
+void FTestRenderPass::OnImguiRender()
+{
+	if (!ImGui::Begin("Vertex Editor"))
+	{
+		ImGui::End();
+		return;
+	}
+
+	if (ImGui::BeginTable("VertexTable", 5, ImGuiTableFlags_Borders | ImGuiTableFlags_RowBg | ImGuiTableFlags_Resizable))
+	{
+		ImGui::TableSetupColumn("ID", ImGuiTableColumnFlags_WidthFixed, 30.0f);
+		ImGui::TableSetupColumn("X", ImGuiTableColumnFlags_WidthFixed, 60.0f);
+		ImGui::TableSetupColumn("Y", ImGuiTableColumnFlags_WidthFixed, 60.0f);
+		ImGui::TableSetupColumn("Z", ImGuiTableColumnFlags_WidthFixed, 60.0f);
+		ImGui::TableSetupColumn("Actions");
+		ImGui::TableHeadersRow();
+
+		for (size64 Index = 0; Index < Vertices.size(); Index++)
+		{
+			FVertex& Vertex = Vertices.at(Index);
+			ImGui::TableNextRow();
+
+			ImGui::TableSetColumnIndex(0);
+			ImGui::Text("%zu", Index);
+
+			ImGui::TableSetColumnIndex(1);
+			ImGui::PushID(static_cast<int>(Index * 10 + 0));
+			ImGui::PushItemWidth(60.0f);
+			ImGui::DragFloat("##X", &Vertex.Positon[0], 0.01f);
+			ImGui::PopID();
+
+			ImGui::TableSetColumnIndex(2);
+			ImGui::PushID(static_cast<int>(Index * 10 + 1));
+			ImGui::PushItemWidth(60.0f);
+			ImGui::DragFloat("##Y", &Vertex.Positon[1], 0.01f);
+			ImGui::PopID();
+
+			ImGui::TableSetColumnIndex(3);
+			ImGui::PushID(static_cast<int>(Index * 10 + 2));
+			ImGui::PushItemWidth(60.0f);
+			ImGui::DragFloat("##Z", &Vertex.Positon[2], 0.01f);
+			ImGui::PopID();
+
+			ImGui::TableSetColumnIndex(4);
+			ImGui::PushID(static_cast<int>(Index * 10 + 6));
+			if (ImGui::Button("Color"))
+			{
+				ImGui::OpenPopup("ColorPicker");
+			}
+
+			if (ImGui::BeginPopup("ColorPicker"))
+			{
+				ImGui::ColorPicker3("##picker", Vertex.Color);
+				ImGui::EndPopup();
+			}
+
+			ImGui::SameLine();
+			ImGui::PopID();
+		}
+		ImGui::EndTable();
+	}
+
+	if (ImGui::Button("Apply"))
+	{
+		VertexBuffer->SetData(Vertices.data(), sizeof(FVertex) * Vertices.size());
+	}
+
+	ImGui::End();
 }
--- Source/Runtime/Engine/Public/Renderwerk/Graphics/RenderPass/TestRenderPass.hpp
+++ Source/Runtime/Engine/Public/Renderwerk/Graphics/RenderPass/TestRenderPass.hpp
@@ -1,11 +1,19 @@
 ﻿#pragma once
 
+#include "Renderwerk/Core/Containers/Vector.hpp"
+#include "Renderwerk/Core/Delegates/MulticastDelegate.hpp"
 #include "Renderwerk/Graphics/RenderPass/GfxRenderPass.hpp"
+
+struct ENGINE_API FVertex
+{
+	float32 Positon[3];
+	float32 Color[3];
+};
 
 class ENGINE_API FTestRenderPass : public IGfxRenderPass
 {
 public:
-	explicit FTestRenderPass(FGfxDevice* InGfxDevice, const FStringView& InDebugName);
+	explicit FTestRenderPass(FGfxDevice* InGfxDevice);
 	~FTestRenderPass() override;
 
 	NON_COPY_MOVEABLE(FTestRenderPass)
@@ -20,7 +28,16 @@ public:
 private:
 	void CreateRenderTarget(const TRef<FGfxResourceManager>& ResourceManager, uint32 Width, uint32 Height);
 
+	void OnImguiRender();
+
 private:
 	TRef<FGfxGraphicsPipeline> GraphicsPipeline;
 	TRef<FGfxTexture> RenderTarget;
+
+	TVector<FVertex> Vertices;
+	TVector<uint32> Indices;
+	TRef<FGfxBuffer> VertexBuffer;
+	TRef<FGfxBuffer> IndexBuffer;
+
+	FDelegateHandle TestHandle;
 };
