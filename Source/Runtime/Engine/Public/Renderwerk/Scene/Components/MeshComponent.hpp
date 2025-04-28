19c8e418fe8007216815b389ad3788dfca326e19
Drischdaan <drischdaan@gmail.com>
2025-04-28 19:56:50 +0200

    feat(engine): adds mesh component class

---
+++ Source/Runtime/Engine/Public/Renderwerk/Scene/Components/MeshComponent.hpp
@@ -0,0 +1,20 @@
+﻿#pragma once
+
+#include "Renderwerk/Core/Containers/Vector.hpp"
+#include "Renderwerk/Core/Memory/SmartPointer.hpp"
+
+class FGfxBuffer;
+
+struct ENGINE_API FVertex
+{
+	float32 Position[3];
+	float32 Color[3];
+};
+
+struct ENGINE_API FMeshComponent
+{
+	TVector<FVertex> Vertices;
+	TVector<uint32> Indices;
+	TRef<FGfxBuffer> VertexBuffer;
+	TRef<FGfxBuffer> IndexBuffer;
+};
