6d0daf7fdb74bf388368d8ba25218d654cd1e1ee
Drischdaan <drischdaan@gmail.com>
2025-04-28 19:56:18 +0200

    feat(engine): adds scene module class

---
+++ Source/Runtime/Engine/Private/Renderwerk/Scene/SceneModule.cpp
@@ -0,0 +1,64 @@
+﻿#include "pch.hpp"
+
+#include "Renderwerk/Scene/SceneModule.hpp"
+
+#include "Renderwerk/Scene/Entity.hpp"
+#include "Renderwerk/Scene/Scene.hpp"
+#include "Renderwerk/Scene/Components/MeshComponent.hpp"
+
+FSceneModule::FSceneModule()
+	: IEngineModule(EEngineThreadAffinity::Update)
+{
+}
+
+void FSceneModule::SetActiveScene(const TRef<FScene>& Scene)
+{
+	if (ActiveScene)
+	{
+		ActiveScene->Unload();
+	}
+	ActiveScene = Scene;
+	Scene->Load();
+}
+
+void FSceneModule::Initialize()
+{
+	SetActiveScene(NewRef<FScene>());
+	{
+		FEntity Entity = ActiveScene->CreateEntity("Test2");
+		FMeshComponent& MeshComponent = Entity.AddComponent<FMeshComponent>();
+
+		const TVector<FVertex> Vertices = {
+			FVertex{.Position = {1.0f, -1.0f, 0.0f}, .Color = {0.0f, 0.0f, 0.0f}},
+			FVertex{.Position = {-1.0f, -1.0f, 0.0f}, .Color = {0.0f, 0.0f, 0.0f}},
+			FVertex{.Position = {0.0f, 1.0f, 0.0f}, .Color = {0.0f, 0.0f, 0.0f}},
+		};
+		MeshComponent.Vertices = Vertices;
+
+		const TVector<uint32> Indices = {0, 1, 2};
+		MeshComponent.Indices = Indices;
+	}
+	{
+		FEntity Entity = ActiveScene->CreateEntity("Test");
+		FMeshComponent& MeshComponent = Entity.AddComponent<FMeshComponent>();
+
+		const TVector<FVertex> Vertices = {
+			FVertex{.Position = {0.5f, -0.5f, 0.0f}, .Color = {1.0f, 1.0f, 1.0f}},
+			FVertex{.Position = {-0.5f, -0.5f, 0.0f}, .Color = {1.0f, 1.0f, 1.0f}},
+			FVertex{.Position = {0.0f, 0.5f, 0.0f}, .Color = {1.0f, 1.0f, 1.0f}},
+		};
+		MeshComponent.Vertices = Vertices;
+
+		const TVector<uint32> Indices = {0, 1, 2};
+		MeshComponent.Indices = Indices;
+	}
+}
+
+void FSceneModule::Shutdown()
+{
+	if (ActiveScene)
+	{
+		ActiveScene->Unload();
+	}
+	ActiveScene.reset();
+}
---
+++ Source/Runtime/Engine/Public/Renderwerk/Scene/SceneModule.hpp
@@ -0,0 +1,32 @@
+﻿#pragma once
+
+#include "Renderwerk/Core/CoreAPI.hpp"
+#include "Renderwerk/Core/Memory/SmartPointer.hpp"
+#include "Renderwerk/Engine/EngineModule.hpp"
+
+class FScene;
+
+class ENGINE_API FSceneModule : public IEngineModule
+{
+public:
+	FSceneModule();
+	~FSceneModule() override = default;
+
+	NON_COPY_MOVEABLE(FSceneModule)
+
+public:
+	void SetActiveScene(const TRef<FScene>& Scene);
+
+public:
+	[[nodiscard]] TRef<FScene> GetActiveScene() const { return ActiveScene; }
+
+private:
+	void Initialize() override;
+	void Shutdown() override;
+
+public:
+	DEFINE_MODULE_NAME("Scene")
+
+private:
+	TRef<FScene> ActiveScene;
+};
