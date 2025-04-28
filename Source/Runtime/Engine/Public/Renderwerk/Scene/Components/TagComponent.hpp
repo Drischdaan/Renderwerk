ce68b810e35684621b5bad8c7496dd2101be5ef0
Drischdaan <drischdaan@gmail.com>
2025-04-28 19:56:32 +0200

    feat(engine): adds tag component class

---
+++ Source/Runtime/Engine/Public/Renderwerk/Scene/Components/TagComponent.hpp
@@ -0,0 +1,14 @@
+﻿#pragma once
+
+#include "Renderwerk/Core/CoreAPI.hpp"
+#include "Renderwerk/Core/Containers/String.hpp"
+
+struct ENGINE_API FTagComponent
+{
+	FAnsiString Name;
+
+	explicit FTagComponent(const FAnsiString& Name)
+		: Name(Name)
+	{
+	}
+};
