53b2965d46a29185bcab198e963522bd9fa1c3a4
Drischdaan <drischdaan@gmail.com>
2025-04-29 21:42:49 +0200

    build(repository): adds assimp includes and binary copying to engine

--- Build/Config/Projects/Engine.lua
+++ Build/Config/Projects/Engine.lua
@@ -10,6 +10,23 @@ rw_module(name)
 		('{COPY} ' .. path.join(agility_path, 'd3d12SDKLayers.dll') .. ' "' .. agility_output_path .. '"'),
 	})
 
+	local output_path = path.join(binaries_path, 'Engine', '%{cfg.buildcfg}');
+	rw_filter_config(build_configs.Debug)
+		postbuildcommands({
+			('{COPY} ' .. rw_thirdparty_module_location('assimp', 'bin', 'Debug', 'assimp-vc143-mtd.dll') .. ' "' .. output_path .. '"'),
+		})
+	rw_filter_end()
+	rw_filter_config(build_configs.Development)
+		postbuildcommands({
+			('{COPY} ' .. rw_thirdparty_module_location('assimp', 'bin', 'Debug', 'assimp-vc143-mtd.dll') .. ' "' .. output_path .. '"'),
+		})
+	rw_filter_end()
+	rw_filter_config(build_configs.Shipping)
+		postbuildcommands({
+			('{COPY} ' .. rw_thirdparty_module_location('assimp', 'bin', 'Release', 'assimp-vc143-mt.dll') .. ' "' .. output_path .. '"'),
+		})
+	rw_filter_end()
+
 	usage('PUBLIC')
 		rw_link_module('mimalloc')
 		rw_link_module('spdlog')
@@ -27,6 +44,33 @@ rw_module(name)
 			rw_thirdparty_module_location('DirectXShaderCompiler', 'lib'),
 		})
 
+		rw_filter_config(build_configs.Debug)
+			libdirs({
+				rw_thirdparty_module_location('assimp', 'bin', 'Debug'),
+			})
+			links({
+				'assimp-vc143-mtd.lib',
+			})
+		rw_filter_end()
+
+		rw_filter_config(build_configs.Development)
+			libdirs({
+				rw_thirdparty_module_location('assimp', 'bin', 'Debug'),
+			})
+			links({
+				'assimp-vc143-mtd.lib',
+			})
+		rw_filter_end()
+
+		rw_filter_config(build_configs.Shipping)
+			libdirs({
+				rw_thirdparty_module_location('assimp', 'bin', 'Release'),
+			})
+			links({
+				'assimp-vc143-mt.lib',
+			})
+		rw_filter_end()
+
 		defines({
 			'GLM_ENABLE_EXPERIMENTAL',
 			'GLM_FORCE_LEFT_HANDED',
@@ -38,4 +82,5 @@ rw_module(name)
 			rw_thirdparty_module_location('DirectXShaderCompiler', 'include'),
 			rw_thirdparty_module_location('entt', 'src'),
 			rw_thirdparty_module_location('glm'),
+			rw_thirdparty_module_location('assimp', 'include'),
 		})
